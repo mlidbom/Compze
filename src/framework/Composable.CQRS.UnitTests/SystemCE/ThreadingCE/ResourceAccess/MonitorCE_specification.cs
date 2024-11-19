d52c0a83c4f82506857fd64fecb6784873a23a12
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-19 12:52:37 +0100

    Allow more time for test to run in case of a lot of contention.

--- src/framework/Composable.CQRS.UnitTests/SystemCE/ThreadingCE/ResourceAccess/MonitorCE_specification.cs
+++ src/framework/Composable.CQRS.UnitTests/SystemCE/ThreadingCE/ResourceAccess/MonitorCE_specification.cs
@@ -63,13 +63,13 @@ namespace Composable.Tests.SystemCE.ThreadingCE.ResourceAccess
             [Test] public void If_owner_thread_blocks_for_less_than_fetchStackTraceTimeout_Exception_contains_owning_threads_stack_trace()
                 => RunWithChangedStackTraceTimeout(
                     fetchStackTraceTimeout: 60.Milliseconds(),
-                    () => RunScenario(ownerThreadBlockTime: 30.Milliseconds(), monitorTimeout: 5.Milliseconds()).Message.Should().Contain(nameof(DisposeOwningThreadLock)));
+                    () => RunScenario(ownerThreadBlockTime: 30.Milliseconds(), monitorTimeout: 10.Milliseconds()).Message.Should().Contain(nameof(DisposeOwningThreadLock)));
 
             [Test] public void If_owner_thread_blocks_for_more_than_fetchStackTraceTimeout_Exception_does_not_contain_owning_threads_stack_trace()
             {
                 RunWithChangedStackTraceTimeout(
-                    fetchStackTraceTimeout: 10.Milliseconds(),
-                    () => RunScenario(ownerThreadBlockTime:20.Milliseconds(), monitorTimeout: 5.Milliseconds()).Message.Should().NotContain(nameof(DisposeOwningThreadLock)));
+                    fetchStackTraceTimeout: 20.Milliseconds(),
+                    () => RunScenario(ownerThreadBlockTime:40.Milliseconds(), monitorTimeout: 10.Milliseconds()).Message.Should().NotContain(nameof(DisposeOwningThreadLock)));
             }
 
             internal static void DisposeOwningThreadLock(IDisposable disposable) => disposable.Dispose();
