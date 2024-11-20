2b5aecf35538fc01b135432a4e5347932c1408b9
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-20 15:24:21 +0100

    style adjustment.

--- src/framework/Composable.CQRS/SystemCE/TransactionsCE/TransactionScopeCE.cs
+++ src/framework/Composable.CQRS/SystemCE/TransactionsCE/TransactionScopeCE.cs
@@ -6,37 +6,34 @@ namespace Composable.SystemCE.TransactionsCE;
 
 public static class TransactionScopeCe
 {
-    internal static void SuppressAmbientAndExecuteInNewTransaction(Action action) => SuppressAmbient(() =>Execute(action));
+   internal static void SuppressAmbientAndExecuteInNewTransaction(Action action) => SuppressAmbient(() => Execute(action));
 
-    internal static TResult SuppressAmbientAndExecuteInNewTransaction<TResult>([InstantHandle]Func<TResult> action) => SuppressAmbient(() => Execute(action));
+   internal static TResult SuppressAmbientAndExecuteInNewTransaction<TResult>([InstantHandle] Func<TResult> action) => SuppressAmbient(() => Execute(action));
 
-    internal static void SuppressAmbient(Action action) => Execute(action, TransactionScopeOption.Suppress);
+   internal static void SuppressAmbient(Action action) => Execute(action, TransactionScopeOption.Suppress);
 
-    internal static TResult SuppressAmbient<TResult>([InstantHandle]Func<TResult> action) => Execute(action, TransactionScopeOption.Suppress);
+   internal static TResult SuppressAmbient<TResult>([InstantHandle] Func<TResult> action) => Execute(action, TransactionScopeOption.Suppress);
 
-    internal static TResult Execute<TResult>([InstantHandle]Func<TResult> action, TransactionScopeOption option = TransactionScopeOption.Required, IsolationLevel isolationLevel = IsolationLevel.Serializable)
-    {
-        TResult result;
-        using (var transaction = CreateScope(option, isolationLevel))
-        {
-            result = action();
-            transaction.Complete();
-        }
-        return result;
-    }
+   internal static TResult Execute<TResult>([InstantHandle] Func<TResult> action, TransactionScopeOption option = TransactionScopeOption.Required, IsolationLevel isolationLevel = IsolationLevel.Serializable)
+   {
+      using var transaction = CreateScope(option, isolationLevel);
+      var result = action();
+      transaction.Complete();
+      return result;
+   }
 
-    internal static void Execute([InstantHandle]Action action, TransactionScopeOption option = TransactionScopeOption.Required, IsolationLevel isolationLevel = IsolationLevel.Serializable)
-    {
-        using var transaction = CreateScope(option, isolationLevel);
-        action();
-        transaction.Complete();
-    }
+   internal static void Execute([InstantHandle] Action action, TransactionScopeOption option = TransactionScopeOption.Required, IsolationLevel isolationLevel = IsolationLevel.Serializable)
+   {
+      using var transaction = CreateScope(option, isolationLevel);
+      action();
+      transaction.Complete();
+   }
 
-    static TransactionScope CreateScope(TransactionScopeOption options, IsolationLevel isolationLevel) =>
-        new(options,
-            new TransactionOptions
-            {
-                IsolationLevel = isolationLevel
-            },
-            TransactionScopeAsyncFlowOption.Enabled);
+   static TransactionScope CreateScope(TransactionScopeOption options, IsolationLevel isolationLevel) =>
+      new(options,
+          new TransactionOptions
+          {
+             IsolationLevel = isolationLevel
+          },
+          TransactionScopeAsyncFlowOption.Enabled);
 }
