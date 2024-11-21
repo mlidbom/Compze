a6c64df7941a3a2b44f07a70c73cb02bfaba2342
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-21 21:15:28 +0100

    Fix oddly behaving slow test

--- src/framework/Composable.CQRS.UnitTests/SystemCE/ThreadingCE/ResourceAccess/MonitorCE_specification.cs
+++ src/framework/Composable.CQRS.UnitTests/SystemCE/ThreadingCE/ResourceAccess/MonitorCE_specification.cs
@@ -56,8 +56,7 @@ namespace Composable.Tests.SystemCE.ThreadingCE.ResourceAccess;
 
    [TestFixture] public class An_exception_is_thrown_by_EnterUpdateLock_if_lock_is_not_acquired_within_timeout
    {
-      [NCrunch.Framework.RestrictToString(false)]
-      [Test] public void Exception_is_ObjectLockTimedOutException() =>
+      [Test,EnableRdi(false)] public void Exception_is_ObjectLockTimedOutException() =>
          RunScenario(ownerThreadBlockTime:20.Milliseconds(), monitorTimeout: 10.Milliseconds()).Should().BeOfType<EnterLockTimeoutException>();
 
       [Test,EnableRdi(false)] public void If_owner_thread_blocks_for_less_than_fetchStackTraceTimeout_Exception_contains_owning_threads_stack_trace() =>
