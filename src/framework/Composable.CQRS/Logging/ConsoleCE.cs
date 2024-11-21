7bfbcc1e81e082cd35be377db6064957739bd24e
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-21 18:23:41 +0100

    We are seeing odd issues that seem to indicate that we may be encountering some sort o deadlock in the ConsoleCE class, farfetched as it seems. Since writing to the console should hardly take long, cut the lock timeout to 2 seconds.

--- src/framework/Composable.CQRS/Logging/ConsoleCE.cs
+++ src/framework/Composable.CQRS/Logging/ConsoleCE.cs
@@ -8,7 +8,7 @@ namespace Composable.Logging;
 static class ConsoleCE
 {
    //NSpec breaks System.Console somehow when tests run in parallel. We are forced to synchronize these tests with other tests and this is the current workaround.
-   static readonly MonitorCE Monitor = MonitorCE.WithDefaultTimeout();
+   static readonly MonitorCE Monitor = MonitorCE.WithTimeout(2.Seconds()); //We are just writing to the console. If this blocks for more than a second or so something is very wrong;
 
    internal static void WriteWarningLine(string message) => WriteLine($"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! {message}");
    internal static void WriteImportantLine(string message) => WriteLine($"############################## {message}");
