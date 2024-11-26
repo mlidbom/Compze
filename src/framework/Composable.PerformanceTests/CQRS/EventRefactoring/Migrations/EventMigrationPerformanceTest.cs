9289cfa46e611435a813a831308c3ad883593f47
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-27 00:13:23 +0100

    Fix resource leak.

--- src/framework/Composable.PerformanceTests/CQRS/EventRefactoring/Migrations/EventMigrationPerformanceTest.cs
+++ src/framework/Composable.PerformanceTests/CQRS/EventRefactoring/Migrations/EventMigrationPerformanceTest.cs
@@ -44,7 +44,11 @@ public class EventMigrationPerformanceTest : EventMigrationTestBase
       _container.ExecuteTransactionInIsolatedScope(() => _container.Resolve<IEventStore>().SaveSingleAggregateEvents(_history));
    }
 
-   [OneTimeTearDown] public void TearDownTask() => _container?.Dispose();
+   [OneTimeTearDown] public async Task TearDownTask()
+   {
+      if(_container != null)
+         await _container.DisposeAsync().CaF();
+   }
 
    async Task AssertUncachedAndCachedAggregateLoadTimes(TimeSpan maxUncachedLoadTime, TimeSpan maxCachedLoadTime, IReadOnlyList<IEventMigration> migrations)
    {
