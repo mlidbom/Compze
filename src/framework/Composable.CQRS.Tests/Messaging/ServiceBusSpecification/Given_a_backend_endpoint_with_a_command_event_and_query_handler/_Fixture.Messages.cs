32e8f33705ab34067b8091d306ffba8d2f9bdf01
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-26 11:31:59 +0100

    Split fixture into to two files for readability.

---
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Given_a_backend_endpoint_with_a_command_event_and_query_handler/_Fixture.Messages.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Composable.DependencyInjection;
+using Composable.Functional;
+using Composable.GenericAbstractions.Time;
+using Composable.Messaging;
+using Composable.Messaging.Buses;
+using Composable.Messaging.Hypermedia;
+using Composable.Persistence.Common.DependencyInjection;
+using Composable.Persistence.EventStore;
+using Composable.Persistence.EventStore.Aggregates;
+using Composable.SystemCE;
+using Composable.SystemCE.LinqCE;
+using Composable.SystemCE.ThreadingCE.TasksCE;
+using Composable.Testing.Threading;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using Composable.Testing;
+
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable InconsistentNaming for testing
+
+#pragma warning disable IDE1006 //Review OK: Test Naming Styles
+#pragma warning disable CA1724  // Type names should not match namespaces
+#pragma warning disable CA1715  // Interfaces should start with I
+
+namespace Composable.Tests.Messaging.ServiceBusSpecification.Given_a_backend_endpoint_with_a_command_event_and_query_handler;
+
+public partial class Fixture : DuplicateByPluggableComponentTest
+{
+   protected static class MyAggregateEvent
+   {
+      public interface IRoot : IAggregateEvent {}
+      public interface Created : IRoot, IAggregateCreatedEvent {}
+      public interface Updated : IRoot {}
+      public static class Implementation
+      {
+         public class Root : AggregateEvent, IRoot
+         {
+            protected Root() {}
+            protected Root(Guid aggregateId) : base(aggregateId) {}
+         }
+
+         // ReSharper disable once MemberHidesStaticFromOuterClass
+         public class Created : Root, MyAggregateEvent.Created
+         {
+            public Created(Guid aggregateId) : base(aggregateId) {}
+         }
+
+         // ReSharper disable once MemberHidesStaticFromOuterClass
+         public class Updated : Root, MyAggregateEvent.Updated {}
+      }
+   }
+
+   protected class MyAggregate : Aggregate<MyAggregate, MyAggregateEvent.Implementation.Root, MyAggregateEvent.IRoot>
+   {
+      public MyAggregate() : base(new DateTimeNowTimeSource())
+      {
+         RegisterEventAppliers()
+           .IgnoreUnhandled<MyAggregateEvent.IRoot>();
+      }
+
+      internal void Update() => Publish(new MyAggregateEvent.Implementation.Updated());
+
+      internal static void Create(Guid id, ILocalHypermediaNavigator bus)
+      {
+         var created = new MyAggregate();
+         created.Publish(new MyAggregateEvent.Implementation.Created(id));
+         bus.Execute(new ComposableApi().EventStore.Commands.Save(created));
+      }
+   }
+
+   protected class MyCreateAggregateCommand : MessageTypes.Remotable.AtMostOnce.AtMostOnceHypermediaCommand
+   {
+      MyCreateAggregateCommand() : base(DeduplicationIdHandling.Reuse) {}
+
+      internal static MyCreateAggregateCommand Create() => new()
+                                                           {
+                                                              MessageId = Guid.NewGuid(),
+                                                              AggregateId = Guid.NewGuid()
+                                                           };
+
+      public Guid AggregateId { get; set; }
+   }
+
+   protected class MyUpdateAggregateCommand : MessageTypes.Remotable.AtMostOnce.AtMostOnceHypermediaCommand
+   {
+      [UsedImplicitly] MyUpdateAggregateCommand() : base(DeduplicationIdHandling.Reuse) {}
+      public MyUpdateAggregateCommand(Guid aggregateId) : base(DeduplicationIdHandling.Create) => AggregateId = aggregateId;
+      public Guid AggregateId { get; private set; }
+   }
+
+   protected class MyExactlyOnceCommand : MessageTypes.Remotable.ExactlyOnce.Command {}
+
+   protected interface IMyExactlyOnceEvent : IAggregateEvent {}
+   protected class MyExactlyOnceEvent : AggregateEvent, IMyExactlyOnceEvent {}
+   protected class MyQuery : MessageTypes.Remotable.NonTransactional.Queries.Query<MyQueryResult> {}
+   protected class MyQueryResult {}
+   protected class MyAtMostOnceCommand : MessageTypes.Remotable.AtMostOnce.AtMostOnceCommand<MyCommandResult>
+   {
+      protected MyAtMostOnceCommand() : base(DeduplicationIdHandling.Reuse) {}
+      internal static MyAtMostOnceCommand Create() => new() {MessageId = Guid.NewGuid()};
+   }
+
+   protected class MyAtMostOnceCommandWithResult : MessageTypes.Remotable.AtMostOnce.AtMostOnceCommand<MyCommandResult>
+   {
+      MyAtMostOnceCommandWithResult() : base(DeduplicationIdHandling.Reuse) {}
+      internal static MyAtMostOnceCommandWithResult Create() => new() {MessageId = Guid.NewGuid()};
+   }
+   protected class MyCommandResult {}
+}
--- src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Given_a_backend_endpoint_with_a_command_event_and_query_handler/_Fixture.cs
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Given_a_backend_endpoint_with_a_command_event_and_query_handler/_Fixture.cs
@@ -27,7 +27,7 @@ using Composable.Testing;
 
 namespace Composable.Tests.Messaging.ServiceBusSpecification.Given_a_backend_endpoint_with_a_command_event_and_query_handler;
 
-public class Fixture : DuplicateByPluggableComponentTest
+public partial class Fixture : DuplicateByPluggableComponentTest
 {
    static readonly TimeSpan _timeout = 10.Seconds();
    internal ITestingEndpointHost Host;
@@ -125,87 +125,6 @@ public class Fixture : DuplicateByPluggableComponentTest
    protected void CloseGates() => AllGates.ForEach(gate => gate.Close());
 
    protected void OpenGates() => AllGates.ForEach(gate => gate.Open());
-
-   protected static class MyAggregateEvent
-   {
-      public interface IRoot : IAggregateEvent {}
-      public interface Created : IRoot, IAggregateCreatedEvent {}
-      public interface Updated : IRoot {}
-      public static class Implementation
-      {
-         public class Root : AggregateEvent, IRoot
-         {
-            protected Root() {}
-            protected Root(Guid aggregateId) : base(aggregateId) {}
-         }
-
-         // ReSharper disable once MemberHidesStaticFromOuterClass
-         public class Created : Root, MyAggregateEvent.Created
-         {
-            public Created(Guid aggregateId) : base(aggregateId) {}
-         }
-
-         // ReSharper disable once MemberHidesStaticFromOuterClass
-         public class Updated : Root, MyAggregateEvent.Updated {}
-      }
-   }
-
-   protected class MyAggregate : Aggregate<MyAggregate, MyAggregateEvent.Implementation.Root, MyAggregateEvent.IRoot>
-   {
-      public MyAggregate() : base(new DateTimeNowTimeSource())
-      {
-         RegisterEventAppliers()
-           .IgnoreUnhandled<MyAggregateEvent.IRoot>();
-      }
-
-      internal void Update() => Publish(new MyAggregateEvent.Implementation.Updated());
-
-      internal static void Create(Guid id, ILocalHypermediaNavigator bus)
-      {
-         var created = new MyAggregate();
-         created.Publish(new MyAggregateEvent.Implementation.Created(id));
-         bus.Execute(new ComposableApi().EventStore.Commands.Save(created));
-      }
-   }
-
-   protected class MyCreateAggregateCommand : MessageTypes.Remotable.AtMostOnce.AtMostOnceHypermediaCommand
-   {
-      MyCreateAggregateCommand() : base(DeduplicationIdHandling.Reuse) {}
-
-      internal static MyCreateAggregateCommand Create() => new()
-                                                           {
-                                                              MessageId = Guid.NewGuid(),
-                                                              AggregateId = Guid.NewGuid()
-                                                           };
-
-      public Guid AggregateId { get; set; }
-   }
-
-   protected class MyUpdateAggregateCommand : MessageTypes.Remotable.AtMostOnce.AtMostOnceHypermediaCommand
-   {
-      [UsedImplicitly] MyUpdateAggregateCommand() : base(DeduplicationIdHandling.Reuse) {}
-      public MyUpdateAggregateCommand(Guid aggregateId) : base(DeduplicationIdHandling.Create) => AggregateId = aggregateId;
-      public Guid AggregateId { get; private set; }
-   }
-
-   protected class MyExactlyOnceCommand : MessageTypes.Remotable.ExactlyOnce.Command {}
-
-   protected interface IMyExactlyOnceEvent : IAggregateEvent {}
-   protected class MyExactlyOnceEvent : AggregateEvent, IMyExactlyOnceEvent {}
-   protected class MyQuery : MessageTypes.Remotable.NonTransactional.Queries.Query<MyQueryResult> {}
-   protected class MyQueryResult {}
-   protected class MyAtMostOnceCommand : MessageTypes.Remotable.AtMostOnce.AtMostOnceCommand<MyCommandResult>
-   {
-      protected MyAtMostOnceCommand() : base(DeduplicationIdHandling.Reuse) {}
-      internal static MyAtMostOnceCommand Create() => new() {MessageId = Guid.NewGuid()};
-   }
-
-   protected class MyAtMostOnceCommandWithResult : MessageTypes.Remotable.AtMostOnce.AtMostOnceCommand<MyCommandResult>
-   {
-      MyAtMostOnceCommandWithResult() : base(DeduplicationIdHandling.Reuse) {}
-      internal static MyAtMostOnceCommandWithResult Create() => new() {MessageId = Guid.NewGuid()};
-   }
-   protected class MyCommandResult {}
 
    public Fixture(string _) : base(_) {}
 }
