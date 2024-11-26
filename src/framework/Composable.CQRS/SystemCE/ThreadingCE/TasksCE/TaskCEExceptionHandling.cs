75f5e6d8d00d3b8695cdbe4de3c6ac0b94642f87
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-26 17:47:37 +0100

    Create extension to ensure we get the full aggregate exception from a task.

---
+++ src/framework/Composable.CQRS/SystemCE/ThreadingCE/TasksCE/TaskCEExceptionHandling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Composable.SystemCE.ThreadingCE.TasksCE;
+
+static class TaskCEExceptionHandling
+{
+   public static async Task WithAggregateExceptions(this Task task)
+   {
+      try
+      {
+         await task.CaF();
+      }
+      catch(Exception exception)
+      {
+         throw task.Exception ?? new AggregateException(exception);
+      }
+   }
+}
