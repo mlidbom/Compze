3c2d125e39b4f9ba802ad0053900d97b2e05bb35
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-02 04:11:20 +0100

    Tests work.

--- src/framework/Compze.Tests.Unit.Internals/DDD/ValueObjectToString.cs
+++ src/framework/Compze.Tests.Unit.Internals/DDD/ValueObjectToString.cs
@@ -27,29 +27,4 @@ public class ValueObjectToString : UniversalTestBase
    {
       public string Name { get; set; }
    }
-
-   [Test]
-   public void ReturnsHierarchicalDescriptionOfData()
-   {
-      var description = new Root
-                        {
-                           Name = "RootName",
-                           Branch1 = new Branch
-                                     {
-                                        Name = "Branch1Name",
-                                        Leaf1 = new Leaf { Name = "Leaf1Name" },
-                                        Leaf2 = new Leaf { Name = "Leaf2Name" }
-                                     },
-                           Branch2 = new Branch
-                                     {
-                                        Name = "Branch1Name",
-                                        Leaf1 = new Leaf { Name = "Leaf1Name" },
-                                        Leaf2 = new Leaf { Name = "Leaf2Name" }
-                                     }
-                        }.ToString();
-      Assert.That(description, Is.EqualTo(
-                     """Compze.Tests.DDD.ValueObjectToString+Root:{"Name":"RootName","Branch1":{"Name":"Branch1Name","Leaf1":{"Name":"Leaf1Name"},"Leaf2":{"Name":"Leaf2Name"}},"Branch2":{"Name":"Branch1Name","Leaf1":{"Name":"Leaf1Name"},"Leaf2":{"Name":"Leaf2Name"}}}"""
-                       .Replace("\r\n","\n", StringComparison.Ordinal)
-                       .Replace("\n",Environment.NewLine, StringComparison.Ordinal)));//Hack to get things working regardless of checkout line endings
-   }
 }
