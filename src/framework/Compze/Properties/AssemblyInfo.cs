5a7377d1487409c70a7d186b0fb98b4e4b9fa62e
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-02 07:04:25 +0100

    Break out Logging.Serilog project

--- src/framework/Compze/Properties/AssemblyInfo.cs
+++ src/framework/Compze/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@
 
 [assembly: InternalsVisibleTo("Compze.DependencyInjection.SimpleInjector")]
 [assembly: InternalsVisibleTo("Compze.DependencyInjection.Microsoft")]
+[assembly: InternalsVisibleTo("Compze.Logging.Serilog")]
