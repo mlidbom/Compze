8f548274159410120974d707da0b5fd7abecdfaa
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-02 20:44:43 +0100

    Start in on trying to make some sense of which projects use internals of which project

--- src/framework/CommonAssemblyInfo.cs
+++ src/framework/CommonAssemblyInfo.cs
@@ -10,17 +10,3 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("0.5.0.0")]
-
-[assembly: InternalsVisibleTo("Compze.Messaging")]
-[assembly: InternalsVisibleTo("Compze.Testing")]
-[assembly: InternalsVisibleTo("Compze.DependencyInjection")]
-[assembly: InternalsVisibleTo("Compze.Serialization")]
-[assembly: InternalsVisibleTo("Compze.Logging")]
-[assembly: InternalsVisibleTo("Compze.DocumentDb")]
-[assembly: InternalsVisibleTo("Compze.EventStore")]
-[assembly: InternalsVisibleTo("Compze.ExternalDependencies")]
-[assembly: InternalsVisibleTo("Compze.Tests.Internals")]
-[assembly: InternalsVisibleTo("Compze.Tests.Unit.Internals")]
-[assembly: InternalsVisibleTo("Compze.Tests.Performance.Internals")]
-
-[assembly: InternalsVisibleTo("ScratchPad.Internals")]
---
+++ src/framework/ShareInternals.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Compze.Messaging")]
+[assembly: InternalsVisibleTo("Compze.Testing")]
+[assembly: InternalsVisibleTo("Compze.DependencyInjection")]
+[assembly: InternalsVisibleTo("Compze.Serialization")]
+[assembly: InternalsVisibleTo("Compze.Logging")]
+[assembly: InternalsVisibleTo("Compze.DocumentDb")]
+[assembly: InternalsVisibleTo("Compze.EventStore")]
+[assembly: InternalsVisibleTo("Compze.ExternalDependencies")]
+[assembly: InternalsVisibleTo("Compze.Tests.Internals")]
+[assembly: InternalsVisibleTo("Compze.Tests.Unit.Internals")]
+[assembly: InternalsVisibleTo("Compze.Tests.Performance.Internals")]
+
+[assembly: InternalsVisibleTo("ScratchPad.Internals")]
