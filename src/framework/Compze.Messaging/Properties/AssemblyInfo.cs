9d40bd68f8f9429216e5d8ee9f6f4036e9a8bdbf
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-02 21:03:40 +0100

    Checkpoint

--- src/framework/Compze.Messaging/Properties/AssemblyInfo.cs
+++ src/framework/Compze.Messaging/Properties/AssemblyInfo.cs
@@ -1,15 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Compze.Messaging")]
 [assembly: InternalsVisibleTo("Compze.Testing")]
-[assembly: InternalsVisibleTo("Compze.DependencyInjection")]
-[assembly: InternalsVisibleTo("Compze.Serialization")]
-[assembly: InternalsVisibleTo("Compze.Logging")]
-[assembly: InternalsVisibleTo("Compze.DocumentDb")]
-[assembly: InternalsVisibleTo("Compze.EventStore")]
 [assembly: InternalsVisibleTo("Compze.ExternalDependencies")]
 [assembly: InternalsVisibleTo("Compze.Tests.Internals")]
 [assembly: InternalsVisibleTo("Compze.Tests.Unit.Internals")]
 [assembly: InternalsVisibleTo("Compze.Tests.Performance.Internals")]
-
-[assembly: InternalsVisibleTo("ScratchPad.Internals")]
