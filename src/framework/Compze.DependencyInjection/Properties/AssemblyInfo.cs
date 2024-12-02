9b7ce87f2e7a76dcc7ff85b5fe2dddfbaa50893e
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-02 20:56:49 +0100

    Checkpoint

--- src/framework/Compze.DependencyInjection/Properties/AssemblyInfo.cs
+++ src/framework/Compze.DependencyInjection/Properties/AssemblyInfo.cs
@@ -3,17 +3,8 @@
 [assembly: InternalsVisibleTo("Compze.DependencyInjection.SimpleInjector")]
 [assembly: InternalsVisibleTo("Compze.DependencyInjection.Microsoft")]
 
-
 [assembly: InternalsVisibleTo("Compze.Messaging")]
 [assembly: InternalsVisibleTo("Compze.Testing")]
-[assembly: InternalsVisibleTo("Compze.DependencyInjection")]
-[assembly: InternalsVisibleTo("Compze.Serialization")]
-[assembly: InternalsVisibleTo("Compze.Logging")]
-[assembly: InternalsVisibleTo("Compze.DocumentDb")]
-[assembly: InternalsVisibleTo("Compze.EventStore")]
 [assembly: InternalsVisibleTo("Compze.ExternalDependencies")]
 [assembly: InternalsVisibleTo("Compze.Tests.Internals")]
-[assembly: InternalsVisibleTo("Compze.Tests.Unit.Internals")]
 [assembly: InternalsVisibleTo("Compze.Tests.Performance.Internals")]
-
-[assembly: InternalsVisibleTo("ScratchPad.Internals")]
