99465e15082f58709b1f14341cc28a081bc0b6af
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-02 04:00:36 +0100

    Checkpoint Broken

--- src/framework/Compze/TypeId.cs
+++ src/framework/Compze/TypeId.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Compze.Contracts;
-using Newtonsoft.Json;
 
 // ReSharper disable ImpureMethodCallOnReadonlyValueField
 
@@ -8,18 +7,19 @@ namespace Compze;
 
 class TypeId
 {
-   [JsonProperty]internal readonly Guid GuidValue;
+   public Guid GuidValue { get; private set; }
 
    // ReSharper disable once ImpureMethodCallOnReadonlyValueField
    public override string ToString() => GuidValue.ToString();
 
-   [JsonConstructor]internal TypeId(Guid guidValue)
+   public TypeId(Guid guidValue)
    {
       Assert.Argument.Assert(guidValue != Guid.Empty);
       GuidValue = guidValue;
    }
 
    public override bool Equals(object? other) => other is TypeId otherTypeId && otherTypeId.GuidValue.Equals(GuidValue);
+   // ReSharper disable once NonReadonlyMemberInGetHashCode
    public override int GetHashCode() => GuidValue.GetHashCode();
 
    public static bool operator ==(TypeId left, TypeId right) => Equals(left, right);
