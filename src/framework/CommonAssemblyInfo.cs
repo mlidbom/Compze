237bfacd39c4c32272dfa5cb716daad83c970e17
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-02 20:36:45 +0100

    Checkpoint

--- src/framework/CommonAssemblyInfo.cs
+++ src/framework/CommonAssemblyInfo.cs
@@ -1,12 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-// ReSharper disable StringLiteralTypo
 
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 
 [assembly: AssemblyDescription("Compze")]
 [assembly: AssemblyConfiguration("")]
@@ -14,12 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("0.5.0.0")]
 
