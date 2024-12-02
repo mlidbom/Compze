8b87d4fb3bbad90bd9fa47b7c97a43e1b23af2de
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-02 06:36:33 +0100

    Break out DocumentDb project

--- src/framework/CommonAssemblyInfo.cs
+++ src/framework/CommonAssemblyInfo.cs
@@ -28,6 +28,8 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Compze.DependencyInjection")]
 [assembly: InternalsVisibleTo("Compze.Serialization")]
 [assembly: InternalsVisibleTo("Compze.Logging")]
+[assembly: InternalsVisibleTo("Compze.DocumentDb")]
+[assembly: InternalsVisibleTo("Compze.EventStore")]
 [assembly: InternalsVisibleTo("Compze.ExternalDependencies")]
 [assembly: InternalsVisibleTo("Compze.Tests.Internals")]
 [assembly: InternalsVisibleTo("Compze.Tests.Unit.Internals")]
