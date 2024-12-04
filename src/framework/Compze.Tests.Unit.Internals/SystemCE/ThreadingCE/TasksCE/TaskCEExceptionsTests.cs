17c3f65c395040a09c41e1389a5591bab3bdbdd4
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-04 21:28:52 +0100

    checkpoint

--- src/framework/Compze.Tests.Unit.Internals/SystemCE/ThreadingCE/TasksCE/TaskCEExceptionsTests.cs
+++ src/framework/Compze.Tests.Unit.Internals/SystemCE/ThreadingCE/TasksCE/TaskCEExceptionsTests.cs
@@ -5,6 +5,7 @@ using Compze.SystemCE.ThreadingCE.TasksCE;
 using Compze.Testing;
 using FluentAssertions;
 using NUnit.Framework;
+using static FluentAssertions.FluentActions;
 
 namespace Compze.Tests.SystemCE.ThreadingCE.TasksCE;
 
@@ -17,6 +18,7 @@ public class TaskCEExceptionsTests : UniversalTestBase
    }
 
    [Test] public async Task WithAggregateExceptions_throws_aggregate_exception_containing_all_exceptions() =>
-      (await AssertThrows.Async<AggregateException>(async () => await Task.WhenAll(Enumerable.Repeat(1, 10).Select(_ => FailingMethod())).WithAggregateExceptions().CaF()).CaF())
-     .InnerExceptions.Should().HaveCount(10);
+      (await Invoking(async () => await Task.WhenAll(Enumerable.Repeat(1, 10).Select(_ => FailingMethod())).WithAggregateExceptions().CaF())
+            .Should().ThrowAsync<AggregateException>().CaF())
+     .Which.InnerExceptions.Should().HaveCount(10);
 }
