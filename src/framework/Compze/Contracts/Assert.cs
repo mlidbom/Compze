710dbf076993a9fd101385a4f812ffc5688dca5e
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-05 19:08:35 +0100

    Checkpoint

--- src/framework/Compze/Contracts/Assert.cs
+++ src/framework/Compze/Contracts/Assert.cs
@@ -1,26 +1,18 @@
-﻿using System;
-using Compze.Contracts.Deprecated;
-
-#pragma warning disable CA1815 // Override equals and operator equals on value types
+﻿#pragma warning disable CA1815 // Override equals and operator equals on value types
 
 namespace Compze.Contracts;
 
 static class Assert
 {
    ///<summary>Assert conditions about current state of "this". Failures would mean that someone made a call that is illegal given state of "this".</summary>
-   public static BaseAssertion State { get; } = BaseAssertion.StateInstance;
+   public static ContractAssertion State { get; } = new StateAssertion();
 
    ///<summary>Assert something that must always be true for "this".</summary>
-   public static BaseAssertion Invariant { get; } = BaseAssertion.InvariantInstance;
+   public static ContractAssertion Invariant { get; } = new InvariantAssertion();
 
    ///<summary>Assert conditions on arguments to current method.</summary>
-   public static BaseAssertion Argument { get; } = BaseAssertion.ArgumentsInstance;
+   public static ContractAssertion Argument { get; } = new ArgumentAssertion();
 
    ///<summary>Assert conditions on the result of making a method call.</summary>
-   public static BaseAssertion Result { get; } = BaseAssertion.ResultInstance;
-
-   public class AssertionException : Exception
-   {
-      public AssertionException(InspectionType inspectionType, int index) : base($"{inspectionType}: {index}") { }
-   }
+   public static ContractAssertion Result { get; } = new ResultAssertion();
 }
---
+++ src/framework/Compze/Contracts/ContractAssertion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using Compze.Contracts.Deprecated;
+
+namespace Compze.Contracts;
+
+abstract class ContractAssertion
+{
+   protected abstract Exception CreateException(string message);
+
+   public ContractAssertion Is([DoesNotReturnIf(false)] bool value, [CallerArgumentExpression(nameof(value))] string valueString = "") =>
+      value ? this : throw CreateException(valueString);
+
+   public TValue NotNull<TValue>(TValue? value, [CallerArgumentExpression(nameof(value))] string valueString = "") =>
+      value ?? throw CreateException(valueString);
+
+   public TValue NotNullOrDefault<TValue>(TValue? value, [CallerArgumentExpression(nameof(value))] string valueString = "")
+      => !NullOrDefaultTester<TValue>.IsNullOrDefault(value) ? value! : throw CreateException(valueString);
+}
+
+class ArgumentAssertion : ContractAssertion
+{
+   protected override Exception CreateException(string message) => new ArgumentException(message);
+}
+
+class InvariantAssertion : ContractAssertion
+{
+   protected override Exception CreateException(string message) => new InvariantException(message);
+}
+
+class StateAssertion : ContractAssertion
+{
+   protected override Exception CreateException(string message) => new InvalidOperationException(message);
+}
+
+class ResultAssertion : ContractAssertion
+{
+   protected override Exception CreateException(string message) => new AssertionException(message);
+}
+
+class InvariantException(string message) : Exception(message);
+class AssertionException(string message) : Exception(message);
