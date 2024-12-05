d1f39471adaa54b7a26e087829b44848d2434c55
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-05 21:26:13 +0100

    checkpoint

--- src/framework/Compze.DependencyInjection.Microsoft/DependencyInjection/Microsoft/MicrosoftDependencyInjectionContainer .cs
+++ src/framework/Compze.DependencyInjection.Microsoft/DependencyInjection/Microsoft/MicrosoftDependencyInjectionContainer .cs
@@ -43,7 +43,7 @@ public sealed class MicrosoftDependencyInjectionContainer : IDependencyInjection
 
          if(componentRegistration.InstantiationSpec.SingletonInstance != null)
          {
-            Assert.Argument.Is(lifetime == ServiceLifetime.Singleton, "Instance can only be used with singletons.");
+            Assert.Argument.Is(lifetime == ServiceLifetime.Singleton, () => $"{componentRegistration.ServiceTypes.First().FullName} tried to register using an Instance and lifestyle: {lifetime}. Instance can only be used with {nameof(Lifestyle.Singleton)}");
             foreach(var serviceType in componentRegistration.ServiceTypes)
             {
                _services.AddSingleton(serviceType, componentRegistration.InstantiationSpec.SingletonInstance);
--- src/framework/Compze/Contracts/ContractAsserter.cs
+++ src/framework/Compze/Contracts/ContractAsserter.cs
@@ -8,6 +8,6 @@ partial class ContractAsserter(Func<string, Exception> createException)
 {
    readonly Func<string, Exception> _createException = createException;
 
-   public ContractAsserter Is([DoesNotReturnIf(false)] bool value, [CallerArgumentExpression(nameof(value))] string valueString = "") =>
-      value ? this : throw _createException(valueString);
+   public ContractAsserter Is([DoesNotReturnIf(false)] bool value, Func<string>? createMessage = null,  [CallerArgumentExpression(nameof(value))] string valueString = "") =>
+      value ? this : throw _createException( createMessage?.Invoke() ?? valueString);
 }
