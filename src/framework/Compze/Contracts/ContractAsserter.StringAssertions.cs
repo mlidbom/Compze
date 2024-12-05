56860bbae034991bb118b3de6d6a8888641cf1dd
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-05 23:15:22 +0100

    checkpoint

--- src/framework/Compze/Contracts/ContractAsserter.StringAssertions.cs
+++ src/framework/Compze/Contracts/ContractAsserter.StringAssertions.cs
@@ -5,6 +5,9 @@ namespace Compze.Contracts;
 
 partial class ContractAsserter
 {
+   public ContractAsserter NotNullOrEmpty(string value, [CallerArgumentExpression(nameof(value))] string valueString = "") =>
+      !string.IsNullOrEmpty(value) ? this : throw _createException($"{valueString} was '{value}' which is {nameof(NotNullEmptyOrWhitespace)}");
+
    public ContractAsserter NotNullEmptyOrWhitespace(string value, [CallerArgumentExpression(nameof(value))] string valueString = "") =>
       !value.IsNullEmptyOrWhiteSpace() ? this : throw _createException($"{valueString} was '{value}' which is {nameof(NotNullEmptyOrWhitespace)}");
 }
