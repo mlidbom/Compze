a5f8a089593bf1d385dcbada1fcedcf49d997bc2
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-06 05:58:52 +0100

    Comment

--- src/framework/Compze/Messaging/_MessageTypes.Classes.WrapperEvent.cs
+++ src/framework/Compze/Messaging/_MessageTypes.Classes.WrapperEvent.cs
@@ -39,6 +39,9 @@ static class WrapperEvent
 
    static IReadOnlyDictionary<Type, Func<IEvent, IWrapperEvent<IEvent>>> _wrapperConstructors = new Dictionary<Type, Func<IEvent, IWrapperEvent<IEvent>>>();
 
+   // Todo: The fact that we can wrap like this, without the types of the wrapping events, does that not also mean that we could, eventually, receive events on the bus without having the type information for all the wrapping events to deserialize to?
+   // Note the eventually though! This is not a priority, but certainly something to keep in mind. If we can dig out just the inner event and wrap it like this, a listening endpoint need only know
+   // the types for the inner event that it listens to, not the types in which it is wrapped. Just a heads up so we don't remove this strange code when we implement aggregates more cleanly. This still has great potential...
    public static IWrapperEvent<IEvent> WrapEvent(IEvent theEvent) =>
       WrapperConstructorFor(theEvent.GetType()).Invoke(theEvent);
 
