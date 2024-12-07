43a176c8507a1536beb710ccd98bbb02be8abb16
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-07 08:50:21 +0100

    Checkpoint

--- src/framework/Compze.Tests.Common/Properties/InternalsVisibleTo.cs
+++ src/framework/Compze.Tests.Common/Properties/InternalsVisibleTo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Compze.Tests")]
+[assembly: InternalsVisibleTo("Compze.Tests.Integration")]
 
 [assembly: InternalsVisibleTo("Compze.Tests.Internals")]
 [assembly: InternalsVisibleTo("Compze.Tests.Unit.Internals")]
