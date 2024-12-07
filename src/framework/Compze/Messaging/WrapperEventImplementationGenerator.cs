c6785a63ffea25f31038e1e9cd21226edc19d525
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-07 23:08:02 +0100

    polish

--- src/framework/Compze/Messaging/WrapperEventImplementationGenerator.cs
+++ src/framework/Compze/Messaging/WrapperEventImplementationGenerator.cs
@@ -38,6 +38,9 @@ static class WrapperEventImplementationGenerator
    public static IWrapperEvent<TWrappedEvent> WrapEvent<TWrappedEvent>(TWrappedEvent theEvent) where TWrappedEvent : IEvent =>
       WrapperConstructorCache<IWrapperEvent<TWrappedEvent>, TWrappedEvent>.Constructor(theEvent);
 
+   // Todo: The fact that we can wrap like this, without the types of the wrapping events, does that not also mean that we could, eventually, receive events on the bus without having the type information for all the wrapping events to deserialize to?
+   // Note the eventually though! This is not a priority, but certainly something to keep in mind. If we can dig out just the inner event and wrap it like this, a listening endpoint need only know
+   // the types for the inner event that it listens to, not the types in which it is wrapped. Just a heads up so we don't remove this strange code when we implement aggregates more cleanly. This still has great potential...
    public static Func<IEvent, IWrapperEvent<IEvent>> ConstructorFor(Type wrappedEventType) =>
       Monitor.DoubleCheckedLocking(
          tryGetValue: () => _wrapperConstructors.GetValueOrDefault(wrappedEventType),
