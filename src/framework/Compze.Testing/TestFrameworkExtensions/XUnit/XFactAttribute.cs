584b10aa3c7f13fff00854e1d9f669cae1e9ecff
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-07 21:36:13 +0100

    the tests run and we have invariant checking in place to ensure they will keep runnig if we rename stuff.

--- src/framework/Compze.Testing/TestFrameworkExtensions/XUnit/XFactAttribute.cs
+++ src/framework/Compze.Testing/TestFrameworkExtensions/XUnit/XFactAttribute.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using Compze.SystemCE.ReflectionCE;
 using JetBrains.Annotations;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
+using static Compze.Contracts.Assert;
 
 namespace Compze.Testing.TestFrameworkExtensions.XUnit;
 
@@ -11,8 +13,18 @@ namespace Compze.Testing.TestFrameworkExtensions.XUnit;
 /// This attribute will run the test eXclusively for the class that declares the test. It will not be executed when inheriting classes run their tests.
 ///This enables us to use BDD style nested classes with inheritance to achieve specification like testing, without an explosion of duplicated test runs.
 /// </summary>
-[XunitTestCaseDiscoverer("Compze.Testing.TestFrameworkExtensions.XUnit", "Compze.Testing")]
-public class XFactAttribute : FactAttribute {}
+[XunitTestCaseDiscoverer(CompzeTestingTestframeworkExtensionsXUnitXFactDiscovererFullTypeName, XFactAttributeAssembly)]
+sealed class XFactAttribute : FactAttribute
+{
+   const string CompzeTestingTestframeworkExtensionsXUnitXFactDiscovererFullTypeName = "Compze.Testing.TestFrameworkExtensions.XUnit.XFactDiscoverer";
+   const string XFactAttributeAssembly = "Compze.Testing";
+
+   static XFactAttribute()
+   {
+      Invariant.Is(CompzeTestingTestframeworkExtensionsXUnitXFactDiscovererFullTypeName == typeof(XFactDiscoverer).GetFullNameCompilable());
+      Invariant.Is(XFactAttributeAssembly == typeof(XFactDiscoverer).Assembly.GetName().Name);
+   }
+}
 
 [UsedImplicitly] public class XFactDiscoverer(IMessageSink diagnosticMessageSink) : IXunitTestCaseDiscoverer
 {
