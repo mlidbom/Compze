8c499997d7b50861ae7a28827bbfa9713683a5d5
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-08 06:39:58 +0100

    Rename test class and files

--- src/framework/Compze.Tests.Unit/CQRS/Aggregates/NestedEntitiesTests/GuidId/Composite_aggregate_specification.Construct.Add_entity1.Add_entity2.Rename_entity1.cs
+++ src/framework/Compze.Tests.Unit/CQRS/Aggregates/NestedEntitiesTests/GuidId/Composite_aggregate_specification.Construct.Add_entity1.Add_entity2.Rename_entity1.cs
@@ -3,7 +3,7 @@ using FluentAssertions;
 
 namespace Compze.Tests.Unit.CQRS.Aggregates.NestedEntitiesTests.GuidId;
 
-public static partial class NestedEntities_specification
+public static partial class Composite_aggregate_specification
 {
    public partial class After_constructing_root_aggregate_with_name_root_and_slaving_a_query_model_to_the_aggregates_events
    {
