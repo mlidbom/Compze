1cf26295f23e6360b218b4e0add2d3255c16b1c0
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-08 05:56:34 +0100

    The component spec is migrated. Phew

--- src/framework/Compze.Tests.Unit/CQRS/Aggregates/NestedEntitiesTests/GuidId/NestedEntities_specification.Construct.The_component.add_entity1.cs
+++ src/framework/Compze.Tests.Unit/CQRS/Aggregates/NestedEntitiesTests/GuidId/NestedEntities_specification.Construct.The_component.add_entity1.cs
@@ -192,33 +192,53 @@ public static partial class NestedEntities_specification
                         }
                      }
                   }
+
+                  public class After_calling_entity1_Remove : After_calling_entity2_Remove
+                  {
+                     public After_calling_entity1_Remove() => _entity1.Remove();
+
+                     public class The_Entities_collection___ : After_calling_entity1_Remove
+                     {
+                        [XFact] public void Single_throws() => Invoking(() => _component.Entities.Single()).Should().Throw<Exception>();
+                        [XFact] public void InCreationOrder_0_throws() => Invoking(() => _component.Entities.InCreationOrder[0]).Should().Throw<Exception>();
+                        [XFact] public void InCreationOrder_Count_is_0() => _component.Entities.InCreationOrder.Count.Should().Be(0);
+
+                        public class Passing_the_entity1_id_to : The_Entities_collection___
+                        {
+                           [XFact] public void Contains_returns_false() => _component.Entities.Contains(_entity1.Id).Should().Be(false);
+                           [XFact] public void Get_throws() => Invoking(() => _component.Entities.Get(_entity1.Id)).Should().Throw<Exception>();
+                           [XFact] public void Indexer_throws() => Invoking(() => _component.Entities[_entity1.Id]).Should().Throw<Exception>();
+
+                           [XFact] public void TryGet_returns_false_and_the_out_parameter_is_null()
+                           {
+                              _component.Entities.TryGet(_entity1.Id, out var entity1Fetched).Should().BeFalse();
+                              entity1Fetched.Should().Be(null);
+                           }
+                        }
+                     }
+
+                     public class The_QueryModel_Entities_collection_ : After_calling_entity1_Remove
+                     {
+                        [XFact] public void Single_throws() => Invoking(() => _qmComponent.Entities.Single()).Should().Throw<Exception>();
+                        [XFact] public void InCreationOrder_0_throws() => Invoking(() => _qmComponent.Entities.InCreationOrder[0]).Should().Throw<Exception>();
+                        [XFact] public void InCreationOrder_Count_is_0() => _qmComponent.Entities.InCreationOrder.Count.Should().Be(0);
+
+                        public class Passing_the_entity1_id_to : The_QueryModel_Entities_collection_
+                        {
+                           [XFact] public void Contains_returns_false() => _qmComponent.Entities.Contains(_entity1.Id).Should().Be(false);
+                           [XFact] public void Get_throws() => Invoking(() => _qmComponent.Entities.Get(_entity1.Id)).Should().Throw<Exception>();
+                           [XFact] public void Indexer_throws() => Invoking(() => _qmComponent.Entities[_entity1.Id]).Should().Throw<Exception>();
+
+                           [XFact] public void TryGet_returns_false_and_the_out_parameter_is_null()
+                           {
+                              _qmComponent.Entities.TryGet(_entity1.Id, out var qmEntity1Fetched).Should().BeFalse();
+                              qmEntity1Fetched.Should().Be(null);
+                           }
+                        }
+                     }
+                  }
                }
             }
-         }
-
-         [XFact] public void Aggregate_Component_Entity_tests()
-         {
-            var _component = Aggregate.Component;
-            var _qmComponent = QueryModel.Component;
-
-            var _entity1Id = Guid.NewGuid();
-            var _entity1 = _component.AddEntity("entity1", _entity1Id);
-
-            var entity2Id = Guid.NewGuid();
-            var agEntity2 = _component.AddEntity("entity2", entity2Id);
-            _component.Invoking(it => it.AddEntity("entity3", entity2Id)).Should().Throw<Exception>();
-
-            agEntity2.Remove();
-
-            _entity1.Remove();
-            _component.Entities.Contains(_entity1.Id).Should().Be(false);
-            _qmComponent.Entities.Contains(_entity1.Id).Should().Be(false);
-            _component.Entities.InCreationOrder.Count.Should().Be(0);
-            _qmComponent.Entities.InCreationOrder.Count.Should().Be(0);
-            _component.Invoking(it => it.Entities.Get(_entity1.Id)).Should().Throw<Exception>();
-            _qmComponent.Invoking(it => it.Entities.Get(_entity1.Id)).Should().Throw<Exception>();
-            _component.Invoking(it => { _ = it.Entities[_entity1.Id]; }).Should().Throw<Exception>();
-            _qmComponent.Invoking(it => { _ = it.Entities[_entity1.Id]; }).Should().Throw<Exception>();
          }
       }
    }
