aba31e721ebc940af8816ca83674a2d591d6e400
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-12-08 01:33:01 +0100

    checkpoint

---
+++ src/framework/Compze.Tests.Unit.Internals/Contracts/AssertionTestBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Compze.Contracts;
+using FluentAssertions;
+
+namespace Compze.Tests.Unit.Internals.Contracts;
+
+public abstract class AssertionTestBase
+{
+   internal static readonly ContractAsserter Asserter = new(message => new AssertionTestException(message));
+   class AssertionTestException(string message) : Exception(message);
+
+   // ReSharper disable once EntityNameCapturedOnly.Global Yes. Capturing its name is the entire point of passing it :)
+   internal static void ThrowsAndCapturesArgumentExpressionText(Func<ContractAsserter> assertFunc, object value, [CallerArgumentExpression(nameof(value))] string valueExpressionString = "")
+   {
+      FluentActions.Invoking(assertFunc)
+                   .Should().Throw<AssertionTestException>()
+                   .Which.Message
+                   .Should().Contain(valueExpressionString);
+   }
+}
