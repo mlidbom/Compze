e141479b6e326946548b2abd8237516ce8593afb
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-29 12:16:52 +0200

    Remove messed up internals visible to.

--- src/framework/Compze.Persistence.MicrosoftSqlServer/Properties/InternalsVisibleTo.cs
+++ src/framework/Compze.Persistence.MicrosoftSqlServer/Properties/InternalsVisibleTo.cs
@@ -2,6 +2,3 @@
 
 [assembly: InternalsVisibleTo("Compze.Tests.Integration.Internals")]
 [assembly: InternalsVisibleTo("Compze.Tests.Performance.Internals")]
-[assembly: InternalsVisibleTo("Compze.Persistence.PostgreSql")]
-[assembly: InternalsVisibleTo("Compze.Persistence.MySql")]
-[assembly: InternalsVisibleTo("Compze.Persistence.MicrosoftSqlServer")]
