c0b0f79337a2f5dc6cb23a131336c3103da98a07
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-30 00:01:59 +0200

    Refactoring

---
+++ src/framework/Compze.Persistence.MicrosoftSqlServer/Persistence/SqlExceptions.cs
@@ -0,0 +1,16 @@
+﻿using Compze.SystemCE;
+using Microsoft.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using Npgsql;
+
+namespace Compze.Persistence;
+
+//We will most likely want to make higher level policy based on this information, so let's start concentrating it here rather than spreading it everywhere.
+static class SqlExceptions
+{
+   internal static class MsSql
+   {
+      const int PrimaryKeyViolationSqlErrorNumber = 2627;
+      internal static bool IsPrimaryKeyViolation(SqlException e) => e.Number == PrimaryKeyViolationSqlErrorNumber;
+   }
+}
--- src/framework/Compze.Persistence.PostgreSql/Persistence/SqlExceptions.cs
+++ src/framework/Compze.Persistence.PostgreSql/Persistence/SqlExceptions.cs
@@ -8,18 +8,6 @@ namespace Compze.Persistence;
 //We will most likely want to make higher level policy based on this information, so let's start concentrating it here rather than spreading it everywhere.
 static class SqlExceptions
 {
-   internal static class MsSql
-   {
-      const int PrimaryKeyViolationSqlErrorNumber = 2627;
-      internal static bool IsPrimaryKeyViolation(SqlException e) => e.Number == PrimaryKeyViolationSqlErrorNumber;
-   }
-
-   internal static class MySql
-   {
-      const int PrimaryKeyViolationSqlErrorNumber = 1062;
-      internal static bool IsPrimaryKeyViolation(MySqlException e) => e.Data["Server Error Code"] as int? == PrimaryKeyViolationSqlErrorNumber;
-   }
-
    internal static class PgSql
    {
       const int PrimaryKeyViolationSqlErrorNumber = 23505;
