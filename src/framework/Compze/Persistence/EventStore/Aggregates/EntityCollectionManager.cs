4bc7c42aa20ec75de06511b253d1835ca3484bd7
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-01 15:43:12 +0200

    refactoring

--- src/framework/Compze/Persistence/EventStore/Aggregates/EntityCollectionManager.cs
+++ src/framework/Compze/Persistence/EventStore/Aggregates/EntityCollectionManager.cs
@@ -55,39 +55,3 @@ public class EntityCollectionManager<TParent,
         return result;
     }
 }
-
-public class RemovableEntityCollectionManager<TParent,
-                                              TParentEvent,
-                                              TParentEventImplementation,
-                                              TEntity,
-                                              TEntityId,
-                                              TEntityEventImplementation,
-                                              TEntityEvent,
-                                              TEntityCreatedEvent,
-                                              TEntityRemovedEvent,
-                                              TEntityEventIdGetterSetter>
-    : EntityCollectionManager<TParent, TParentEvent, TParentEventImplementation, TEntity, TEntityId, TEntityEventImplementation, TEntityEvent, TEntityCreatedEvent, TEntityEventIdGetterSetter>
-    where TParent : IEventiveInternals<TParentEventImplementation, TParentEvent>
-    where TParentEvent : class, IAggregateEvent
-    where TParentEventImplementation : AggregateEvent, TParentEvent
-    where TEntityId : notnull
-    where TEntityEvent : class, TParentEvent
-    where TEntityCreatedEvent : TEntityEvent
-    where TEntityRemovedEvent : TEntityEvent
-    where TEntityEventImplementation : TEntityEvent, TParentEventImplementation
-    where TEntity : EventiveComponent<TParent, TParentEvent, TParentEventImplementation, TEntity, TEntityEventImplementation, TEntityEvent>
-    where TEntityEventIdGetterSetter :
-    IGetSetAggregateEntityEventEntityId<TEntityId, TEntityEventImplementation, TEntityEvent>
-{
-    protected RemovableEntityCollectionManager(TParent parent,
-                                               Action<TEntityEventImplementation> raiseEventThroughParent,
-                                               IEventHandlerRegistrar<TEntityEvent> appliersRegistrar)
-        : base(parent, raiseEventThroughParent, appliersRegistrar)
-    {
-        appliersRegistrar.For<TEntityRemovedEvent>(e =>
-        {
-            var id = IdGetter.GetId(e);
-            ManagedEntities.Remove(id);
-        });
-    }
-}
---
+++ src/framework/Compze/Persistence/EventStore/Aggregates/RemovableEntityCollectionManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Compze.Messaging.Events;
+
+namespace Compze.Persistence.EventStore.Aggregates;
+
+public class RemovableEntityCollectionManager<TParent,
+                                              TParentEvent,
+                                              TParentEventImplementation,
+                                              TEntity,
+                                              TEntityId,
+                                              TEntityEventImplementation,
+                                              TEntityEvent,
+                                              TEntityCreatedEvent,
+                                              TEntityRemovedEvent,
+                                              TEntityEventIdGetterSetter>
+    : EntityCollectionManager<TParent, TParentEvent, TParentEventImplementation, TEntity, TEntityId, TEntityEventImplementation, TEntityEvent, TEntityCreatedEvent, TEntityEventIdGetterSetter>
+    where TParent : IEventiveInternals<TParentEventImplementation, TParentEvent>
+    where TParentEvent : class, IAggregateEvent
+    where TParentEventImplementation : AggregateEvent, TParentEvent
+    where TEntityId : notnull
+    where TEntityEvent : class, TParentEvent
+    where TEntityCreatedEvent : TEntityEvent
+    where TEntityRemovedEvent : TEntityEvent
+    where TEntityEventImplementation : TEntityEvent, TParentEventImplementation
+    where TEntity : EventiveComponent<TParent, TParentEvent, TParentEventImplementation, TEntity, TEntityEventImplementation, TEntityEvent>
+    where TEntityEventIdGetterSetter :
+    IGetSetAggregateEntityEventEntityId<TEntityId, TEntityEventImplementation, TEntityEvent>
+{
+    protected RemovableEntityCollectionManager(TParent parent,
+                                               Action<TEntityEventImplementation> raiseEventThroughParent,
+                                               IEventHandlerRegistrar<TEntityEvent> appliersRegistrar)
+        : base(parent, raiseEventThroughParent, appliersRegistrar)
+    {
+        appliersRegistrar.For<TEntityRemovedEvent>(e =>
+        {
+            var id = IdGetter.GetId(e);
+            ManagedEntities.Remove(id);
+        });
+    }
+}
