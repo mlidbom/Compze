e5240509ce3e60b4e4803be5883fabe9230ae3d6
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-01 15:23:00 +0200

    refactoring

---
+++ src/framework/Compze/Persistence/EventStore/Aggregates/EventiveRemovableEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Compze.Messaging.Events;
+
+namespace Compze.Persistence.EventStore.Aggregates;
+
+public abstract class EventiveRemovableEntity<TParent,
+                                              TParentEvent,
+                                              TParentEventImplementation,
+                                              TEntity,
+                                              TEntityId,
+                                              TEntityEventImplementation,
+                                              TEntityEvent,
+                                              TEntityCreatedEvent,
+                                              TEntityRemovedEvent,
+                                              TEntityEventIdGetterSetter>
+    : EventiveEntity<TParent, TParentEvent, TParentEventImplementation, TEntity, TEntityId, TEntityEventImplementation, TEntityEvent, TEntityCreatedEvent, TEntityEventIdGetterSetter>
+    where TParent : IEventiveInternals<TParentEventImplementation, TParentEvent>
+    where TParentEvent : class, IAggregateEvent
+    where TEntityId : struct
+    where TEntityEvent : class, TParentEvent
+    where TParentEventImplementation : AggregateEvent, TParentEvent
+    where TEntityEventImplementation : TParentEventImplementation, TEntityEvent
+    where TEntityCreatedEvent : TEntityEvent
+    where TEntityRemovedEvent : TEntityEvent
+    where TEntity : EventiveRemovableEntity<TParent, TParentEvent, TParentEventImplementation, TEntity, TEntityId, TEntityEventImplementation, TEntityEvent, TEntityCreatedEvent, TEntityRemovedEvent, TEntityEventIdGetterSetter>
+    where TEntityEventIdGetterSetter : IGetSetAggregateEntityEventEntityId<TEntityId, TEntityEventImplementation, TEntityEvent>
+{
+    static EventiveRemovableEntity() => AggregateTypeValidator<TEntity, TEntityEventImplementation, TEntityEvent>.AssertStaticStructureIsValid();
+
+    protected EventiveRemovableEntity(TParent aggregate) : base(aggregate)
+    {
+        RegisterEventAppliers()
+           .IgnoreUnhandled<TEntityRemovedEvent>();
+    }
+
+    public new static CollectionManager CreateSelfManagingCollection(TParent parent)
+        => new(parent, @event => parent.Publish(@event), parent.RegisterEventAppliers());
+
+    public new class CollectionManager : RemovableEntityCollectionManager<TParent, TParentEvent, TParentEventImplementation, TEntity, TEntityId, TEntityEventImplementation, TEntityEvent, TEntityCreatedEvent, TEntityRemovedEvent, TEntityEventIdGetterSetter>
+    {
+        internal CollectionManager(TParent parent, Action<TEntityEventImplementation> raiseEventThroughParent, IEventHandlerRegistrar<TEntityEvent> appliersRegistrar)
+            : base(parent, raiseEventThroughParent, appliersRegistrar) {}
+    }
+}
