15cf64a65486e897baf214349c466efe786a39ab
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-03 03:50:00 +0200

    warning

--- src/framework/Compze/Persistence/DocumentDb/PolymorphicEntityCache.cs
+++ src/framework/Compze/Persistence/DocumentDb/PolymorphicEntityCache.cs
@@ -15,76 +15,67 @@ namespace Compze.Persistence.DocumentDb;
 ///<summary>Tracks entities by converting the supplied id into a string and maintaining a dictionary from that string id to a list of instances to enable polymorphism</summary>
 class PolymorphicEntityIdMap : IEnumerable<KeyValuePair<string, object>>
 {
-   readonly Dictionary<string, List<object>> _stringIdToInstance = new(StringComparer.InvariantCultureIgnoreCase);
-   readonly MonitorCE _monitor = MonitorCE.WithDefaultTimeout();
+    readonly Dictionary<string, List<object>> _stringIdToInstance = new(StringComparer.InvariantCultureIgnoreCase);
+    readonly MonitorCE _monitor = MonitorCE.WithDefaultTimeout();
 
-   internal bool Contains(Type type, object id) => _monitor.Read(() => ContainsInternal(type, id));
+    internal bool Contains(Type type, object id) => _monitor.Read(func: () => ContainsInternal(type, id));
 
-   bool ContainsInternal(Type type, object id) => TryGet(type, id, out _);
+    bool ContainsInternal(Type type, object id) => TryGet(type, id, out _);
 
-   internal bool TryGet<T>(object id, [MaybeNullWhen(false)] out T value)
-   {
-      using(_monitor.TakeUpdateLock())
-      {
-         if(TryGet(typeof(T), id, out var found))
-         {
-            value = (T)found;
-            return true;
-         }
+    internal bool TryGet<T>(object id, [MaybeNullWhen(returnValue: false)] out T value)
+    {
+        using(_monitor.TakeUpdateLock())
+        {
+            if(TryGet(typeof(T), id, out var found))
+            {
+                value = (T)found;
+                return true;
+            }
 
-         value = default!;
-         return false;
-      }
-   }
+            value = default!;
+            return false;
+        }
+    }
 
-   bool TryGet(Type typeOfValue, object id, [NotNullWhen(true)] out object? value)
-   {
-      var idString = GetIdStringRepresentation(id);
-      value = null;
+    bool TryGet(Type typeOfValue, object id, [NotNullWhen(returnValue: true)] out object? value)
+    {
+        var idString = GetIdStringRepresentation(id);
+        value = null;
 
-      if(!_stringIdToInstance.TryGetValue(idString, out var matchesId)) return false;
+        if(!_stringIdToInstance.TryGetValue(idString, out var matchesId)) return false;
 
-      var found = matchesId.Where(typeOfValue.IsInstanceOfType).ToList();
-      if(!found.Any()) return false;
+        var found = matchesId.Where(typeOfValue.IsInstanceOfType).ToList();
+        if(!found.Any()) return false;
 
-      value = found.Single();
-      return true;
-   }
+        value = found.Single();
+        return true;
+    }
 
-   static string GetIdStringRepresentation(object id) => Result.ReturnNotNull(id).ToStringNotNull().ToUpperInvariant().TrimEnd(' ');
+    static string GetIdStringRepresentation(object id) => Result.ReturnNotNull(id).ToStringNotNull().ToUpperInvariant().TrimEnd(trimChar: ' ');
 
-   public virtual void Add<T>(object id, T value) => _monitor.Update(() =>
-   {
-      Argument.NotNull(value);
+    public void Add<T>(object id, T value) => _monitor.Update(action: () =>
+    {
+        Argument.NotNull(value);
 
-      var idString = GetIdStringRepresentation(id);
-      if(ContainsInternal(value.GetType(), idString))
-      {
-         throw new AttemptToSaveAlreadyPersistedValueException(id, value);
-      }
+        var idString = GetIdStringRepresentation(id);
+        if(ContainsInternal(value.GetType(), idString)) throw new AttemptToSaveAlreadyPersistedValueException(id, value);
 
-      _stringIdToInstance.GetOrAddDefault(idString).Add(value);
-   });
+        _stringIdToInstance.GetOrAddDefault(idString).Add(value);
+    });
 
-   public void Remove(object id, Type documentType) => _monitor.Update(() =>
-   {
-      var idString = GetIdStringRepresentation(id);
-      var removed = _stringIdToInstance.GetOrAddDefault(idString).RemoveWhere(documentType.IsInstanceOfType);
-      if(removed.None())
-      {
-         throw new NoSuchDocumentException(id, documentType);
-      }
+    public void Remove(object id, Type documentType) => _monitor.Update(action: () =>
+    {
+        var idString = GetIdStringRepresentation(id);
+        var removed = _stringIdToInstance.GetOrAddDefault(idString).RemoveWhere(documentType.IsInstanceOfType);
+        if(removed.None()) throw new NoSuchDocumentException(id, documentType);
 
-      if(removed.Count > 1)
-      {
-         throw new Exception("It really should be impossible to hit multiple documents with one Id, but apparently you just did it!");
-      }
-   });
+        if(removed.Count > 1) throw new Exception(message: "It really should be impossible to hit multiple documents with one Id, but apparently you just did it!");
+    });
 
-   public IEnumerator<KeyValuePair<string, object>> GetEnumerator() => _monitor.Read(
-      () => _stringIdToInstance.SelectMany(m => m.Value.Select(inner => new KeyValuePair<string, object>(m.Key, inner)))
-               .ToList() //ToList is to make it thread safe...
-               .GetEnumerator());
+    public IEnumerator<KeyValuePair<string, object>> GetEnumerator() => _monitor.Read(
+        func: () => _stringIdToInstance.SelectMany(selector: m => m.Value.Select(selector: inner => new KeyValuePair<string, object>(m.Key, inner)))
+                                       .ToList() //ToList is to make it thread safe...
+                                       .GetEnumerator());
 
-   IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
