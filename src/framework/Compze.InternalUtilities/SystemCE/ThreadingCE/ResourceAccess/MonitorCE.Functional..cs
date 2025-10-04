6d021961f873a39744dcb5e1d588a50855130d6d
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-04 10:54:03 +0200

    fix spurious warning.

--- src/framework/Compze.InternalUtilities/SystemCE/ThreadingCE/ResourceAccess/MonitorCE.Functional..cs
+++ src/framework/Compze.InternalUtilities/SystemCE/ThreadingCE/ResourceAccess/MonitorCE.Functional..cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Compze.Utilities.Functional;
 
 namespace Compze.Utilities.SystemCE.ThreadingCE.ResourceAccess;
 
+[SuppressMessage("Design",
+                 "CA1001:Types that own disposable fields should be disposable",
+                 Justification = "The lock fields are reusable tokens created once in the constructor for zero-allocation operations. They are not traditional disposable resources that need cleanup.")]
 partial class MonitorCE
 {
    public TReturn Read<TReturn>(Func<TReturn> func)
