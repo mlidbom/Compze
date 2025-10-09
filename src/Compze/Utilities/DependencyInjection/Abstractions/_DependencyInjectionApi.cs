3252836ee826e7d994fc1dd694fdd469fe68cc31
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-09 07:27:32 +0200

    cruft.

--- src/Compze/Utilities/DependencyInjection/Abstractions/_DependencyInjectionApi.cs
+++ src/Compze/Utilities/DependencyInjection/Abstractions/_DependencyInjectionApi.cs
@@ -25,43 +25,7 @@ public interface IDependencyInjectionContainer : IDisposable, IAsyncDisposable
    IDependencyRegistrar Register();
    IDependencyInjectionContainer Register(params ComponentRegistration[] registrations);
    IEnumerable<ComponentRegistration> RegisteredComponents();
-   bool IsRegistered<TService>() => RegisteredComponents().Any(c => c.ServiceTypes.Contains(typeof(TService)));
-   bool IsRegistered(Type serviceType) => RegisteredComponents().Any(c => c.ServiceTypes.Contains(serviceType));
    IServiceLocator ServiceLocator { get; }
-
-   /// <summary>
-   /// Registers this container to handle service resolution for an IServiceCollection.
-   /// Returns a custom IServiceProviderFactory that should be used instead of the default.
-   /// This prevents double-disposal of services managed by this container.
-   /// 
-   /// Usage in ASP.NET Core:
-   /// <code>
-   /// builder.Host.UseServiceProviderFactory(new CompzeServiceProviderFactory(container));
-   /// </code>
-   /// </summary>
-   IServiceProviderFactory<IServiceCollection> CreateServiceProviderFactory()
-   {
-      return new CompzeServiceProviderFactory(this);
-   }
-   
-   /// <summary>
-   /// DEPRECATED: This method causes double-disposal issues. Use CreateServiceProviderFactory() instead.
-   /// Registers services from this container into a Microsoft DI ServiceCollection.
-   /// WARNING: Services will be disposed twice - once by Microsoft DI and once by this container.
-   /// </summary>
-   [Obsolete("This method causes double-disposal. Use CreateServiceProviderFactory() instead.")]
-   void RegisterToHandleServiceResolutionFor(IServiceCollection services)
-   {
-      var serviceLocator = ServiceLocator;
-      foreach(var component in RegisteredComponents())
-      {
-         foreach(var serviceType in component.ServiceTypes)
-         {
-            //We handle lifetimes ourselves so registering everything as transient in the other container will avoid duplicate Dispose calls.
-            services.AddTransient(serviceType, _ => component.Resolve(serviceLocator));
-         }
-      }
-   }
 }
 
 public interface IServiceLocator : IDisposable, IAsyncDisposable
