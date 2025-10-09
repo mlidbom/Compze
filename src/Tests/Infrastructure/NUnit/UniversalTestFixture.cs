540e754e45eff259ee1d4c44c27e8e859b890d5b
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-09 12:01:34 +0200

    refactoring

--- src/Tests/Infrastructure/NUnit/UniversalTestFixture.cs
+++ src/Tests/Infrastructure/NUnit/UniversalTestFixture.cs
@@ -1,14 +1,9 @@
 using System;
-using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Compze.Tests.Infrastructure.NUnit.Logging;
-using Compze.Utilities.Logging;
-using Compze.Utilities.Logging.Serilog;
 using Compze.Utilities.SystemCE;
 using NUnit.Framework;
-using Serilog;
-using Serilog.Exceptions;
 
 namespace Compze.Tests.Infrastructure.NUnit;
 
@@ -16,58 +11,21 @@ namespace Compze.Tests.Infrastructure.NUnit;
 {
    [OneTimeSetUp] public void UniversalSetup()
    {
-      SetupSerilog();
-      UncatchableExceptionsGatherer.ForceFullGcAllGenerationsAndWaitForFinalizersConsumeAndThrowAnyGatheredExceptions();
+      TestFixtureHelper.PerformSetup(new NUnitTestEnricher());
       AssertTestInheritsUniversalTestBase();
    }
 
    [OneTimeTearDown] public async Task UniversalTeardown()
    {
-      //We don't consume here,because some test runners, including NCrunch, will not surface teardown exceptions, so consuming here would hide them. Without consuming, we may see them on the next test run.
-      GCCE.ForceFullGcAllGenerationsAndWaitForFinalizers();
-      if(UncatchableExceptionsGatherer.Exceptions.Any())
-      {
-         throw new AggregateException(UncatchableExceptionsGatherer.Exceptions);
-      }
-
-      await Log.CloseAndFlushAsync();
-   }
-
-   static void SetupSerilog()
-   {
-      Log.Logger = new LoggerConfiguration()
-                  .Enrich.WithMachineName()
-                  .Enrich.WithExceptionDetails()
-                  .Enrich.With<NUnitTestEnricher>()
-                  .MinimumLevel.Debug()
-                  .WriteTo.Seq("http://192.168.0.11:5341", formatProvider: CultureInfo.InvariantCulture)
-                  .WriteTo.Console(formatProvider:CultureInfo.InvariantCulture)
-                  .CreateLogger();
-
-      CompzeLogger.LoggerFactoryMethod = SerilogLogger.Create;
+      TestFixtureHelper.PerformTeardown();
+      await Task.CompletedTask; // Keep async signature for consistency
    }
 
    void AssertTestInheritsUniversalTestBase()
    {
-      var testClasses = GetType().Assembly
-                                 .GetTypes()
-                                 .Where(IsTestClass);
-
-      var invalidTests = testClasses.Where(t => !typeof(UniversalTestBase).IsAssignableFrom(t)).ToList();
-
-      if(invalidTests.Any())
-      {
-         var typeList = string.Join(Environment.NewLine, invalidTests.Select(t => t.FullName));
-         Assert.Fail($"The following test classes do not inherit from TestBase: {typeList}: Count{invalidTests.Count}");
-      }
-   }
-
-   static bool IsTestClass(Type type)
-   {
-      if(type.GetCustomAttributes(typeof(TestFixtureAttribute), true).Any())
-         return true;
-
-      return type.GetMethods()
-                 .Any(method => method.GetCustomAttributes(typeof(TestAttribute), true).Any());
+      TestFixtureHelper.AssertAllTestClassesInheritFromBase(
+         GetType().Assembly,
+         typeof(UniversalTestBase),
+         TestFixtureHelper.IsNUnitTestClass);
    }
 }
---
+++ src/Tests/Unit.Internals.XUnit/AssemblyFixture.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Compze.Tests.Unit.Internals.XUnit;
+
+/// <summary>
+/// XUnit v2 doesn't have IAssemblyFixture. This class uses a module initializer to perform assembly-level setup.
+/// </summary>
+public static class AssemblySetup
+{
+   static bool _initialized;
+   static readonly object _lock = new();
+
+   [ModuleInitializer]
+   public static void Initialize()
+   {
+      lock(_lock)
+      {
+         if(_initialized) return;
+         _initialized = true;
+
+         Tests.Infrastructure.TestFixtureHelper.PerformSetup();
+         AssertTestInheritsUniversalTestBase();
+
+         // Register cleanup for when the AppDomain unloads
+         AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+         AppDomain.CurrentDomain.DomainUnload += OnDomainUnload;
+      }
+   }
+
+   static void OnProcessExit(object? sender, EventArgs e) => Cleanup();
+   static void OnDomainUnload(object? sender, EventArgs e) => Cleanup();
+
+   static void Cleanup()
+   {
+      Tests.Infrastructure.TestFixtureHelper.PerformTeardown();
+   }
+
+   static void AssertTestInheritsUniversalTestBase()
+   {
+      Tests.Infrastructure.TestFixtureHelper.AssertAllTestClassesInheritFromBase(
+         typeof(AssemblySetup).Assembly,
+         typeof(Tests.Infrastructure.XUnit.UniversalTestBase),
+         Tests.Infrastructure.TestFixtureHelper.IsXUnitTestClass);
+   }
+}
