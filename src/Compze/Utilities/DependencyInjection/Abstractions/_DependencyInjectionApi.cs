62b5067bee0e0d415325a5a15442de1ebf25aa11
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-09 07:38:54 +0200

    cruft.

--- src/Compze/Utilities/DependencyInjection/Abstractions/_DependencyInjectionApi.cs
+++ src/Compze/Utilities/DependencyInjection/Abstractions/_DependencyInjectionApi.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Compze.Utilities.Functional;
 using Compze.Utilities.SystemCE.LinqCE;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Compze.Utilities.DependencyInjection.Abstractions;
 
