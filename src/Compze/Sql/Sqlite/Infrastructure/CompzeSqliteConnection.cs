4203d75d6e80b992067ffc8a6697275e12ea9931
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-11 17:45:16 +0200

    Improve Sqlite transaction code.

--- src/Compze/Sql/Sqlite/Infrastructure/CompzeSqliteConnection.cs
+++ src/Compze/Sql/Sqlite/Infrastructure/CompzeSqliteConnection.cs
@@ -1,6 +1,7 @@
 using System.Data.Common;
 using System.Transactions;
 using Compze.Sql.Common.Abstractions;
+using Compze.Utilities.Contracts;
 using Compze.Utilities.SystemCE.TransactionsCE;
 using Compze.Utilities.Threading.TasksCE;
 using Microsoft.Data.Sqlite;
@@ -15,78 +16,66 @@ internal interface ICompzeSqliteConnection : IPoolableConnection, ICompzeDbConne
    {
       SqliteConnection Connection { get; }
       SqliteTransaction? _transaction;
-      VolatileLambdaTransactionParticipant? _transactionParticipant;
+      readonly VolatileLambdaTransactionParticipant _transactionParticipant;
 
-      internal CompzeSqliteConnection(string connectionString) => Connection = new SqliteConnection(connectionString);
+      internal CompzeSqliteConnection(string connectionString)
+      {
+         Connection = new SqliteConnection(connectionString);
+
+         _transactionParticipant = new VolatileLambdaTransactionParticipant(
+            enlistmentOptions: EnlistmentOptions.None,
+            onEnlist: () => _transaction = Connection.BeginTransaction(System.Data.IsolationLevel.Serializable),
+            onPrepare: () => {}, // Nothing to do in prepare - SQLite doesn't support two-phase commit
+            onCommit: () =>
+            {
+               _transaction!.Commit();
+               _transaction.Dispose();
+               _transaction = null;
+            },
+            onRollback: () =>
+            {
+               _transaction!.Rollback();
+               _transaction.Dispose();
+               _transaction = null;
+            });
+      }
 
       public void Open()
       {
          Connection.Open();
-         EnlistInAmbientTransaction();
+         _transactionParticipant.EnsureEnlistedInAnyAmbientTransaction();
       }
 
       public async Task OpenAsync()
       {
          await Connection.OpenAsync().caf();
-         EnlistInAmbientTransaction();
-      }
-
-      void EnlistInAmbientTransaction()
-      {
-         var ambientTransaction = Transaction.Current;
-         if(ambientTransaction != null && _transaction == null)
-         {
-            // Start an IMMEDIATE transaction for better concurrency control
-            // IMMEDIATE mode acquires a RESERVED lock immediately, preventing other writers
-            // This is more appropriate for transactional workloads than DEFERRED mode
-            _transaction = Connection.BeginTransaction(System.Data.IsolationLevel.Serializable);
-
-            // Create a participant that will commit or rollback the SQLite transaction
-            _transactionParticipant = new VolatileLambdaTransactionParticipant(
-               enlistmentOptions: EnlistmentOptions.None,
-               onPrepare: () =>
-               {
-                  // Nothing to do in prepare - SQLite doesn't support two-phase commit
-               },
-               onCommit: () =>
-               {
-                  _transaction?.Commit();
-                  _transaction?.Dispose();
-                  _transaction = null;
-               },
-               onRollback: () =>
-               {
-                  _transaction?.Rollback();
-                  _transaction?.Dispose();
-                  _transaction = null;
-               });
-
-            // Enlist in the ambient transaction
-            _transactionParticipant.EnsureEnlistedInAnyAmbientTransaction();
-         }
+         _transactionParticipant.EnsureEnlistedInAnyAmbientTransaction();
       }
 
       DbCommand ICompzeDbConnection.CreateCommand() => CreateCommand();
-      
+
       public SqliteCommand CreateCommand()
       {
+         _transactionParticipant.EnsureEnlistedInAnyAmbientTransaction();
+
          var command = Connection.CreateCommand();
          if(_transaction != null)
          {
             command.Transaction = _transaction;
          }
+
          return command;
       }
 
       public void Dispose()
       {
-         _transaction?.Dispose();
+         Assert.State.Is(_transaction == null, () => "Transaction should have been completed (committed or rolled back) before disposing the connection");
          Connection.Dispose();
       }
 
       public ValueTask DisposeAsync()
       {
-         _transaction?.Dispose();
+         Assert.State.Is(_transaction == null, () => "Transaction should have been completed (committed or rolled back) before disposing the connection");
          return Connection.DisposeAsync();
       }
    }
