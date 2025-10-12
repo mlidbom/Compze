58960b94d193af91f7e961ddd8bd21115e36600c
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-12 17:59:05 +0200

    refactoring

---
+++ src/Compze/Tessaging/Hosting/Testing/DependencyInjection/DiContainerExtensions.cs
@@ -0,0 +1,43 @@
+using System;
+using Compze.Common.Refactoring.Naming.Wiring;
+using Compze.Tessaging.Hosting.Implementation;
+using Compze.Tessaging.Hosting.Testing.Sql;
+using Compze.Utilities.DependencyInjection;
+using Compze.Utilities.DependencyInjection.Abstractions;
+using Compze.Utilities.DependencyInjection.Microsoft;
+using Compze.Utilities.DependencyInjection.SimpleInjector;
+using Compze.Wiring;
+using JetBrains.Annotations;
+
+namespace Compze.Tessaging.Hosting.Testing.DependencyInjection;
+
+static class DiContainerExtensions
+{
+   public static IDependencyInjectionContainer Create(this DIContainer @this, IRunMode runMode)
+   {
+      IDependencyInjectionContainer container = @this switch
+      {
+         DIContainer.SimpleInjector => new SimpleInjectorDependencyInjectionContainer(runMode),
+         DIContainer.Microsoft      => new MicrosoftDependencyInjectionContainer(runMode),
+         _                          => throw new ArgumentOutOfRangeException()
+      };
+
+      container.Register(Singleton.For<IServiceLocator>().CreatedBy(() => container.ServiceLocator));
+      return container;
+   }
+
+   public static IServiceLocator CreateServiceLocatorForTesting(this DIContainer @this, [InstantHandle] Action<IDependencyRegistrar> setup)
+   {
+      var container = @this.Create(RunMode.Testing);
+      container.Register()
+               .TimeSource()
+               .TypeMapper()
+               .DummyConfigurationParameterProvider()
+               .CurrentTestsConfiguredSqlLayer()
+               .MessageHandlerRegistry()
+               .InMemoryEventStoreEventPublisher();
+      setup(container.Register());
+
+      return container.ServiceLocator;
+   }
+}
