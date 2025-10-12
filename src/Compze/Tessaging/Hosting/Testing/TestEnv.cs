ce034e3bc3ce4e9234fe165782a63f1be2d61b78
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-12 20:34:26 +0200

    checkpoint

--- src/Compze/Tessaging/Hosting/Testing/TestEnv.cs
+++ src/Compze/Tessaging/Hosting/Testing/TestEnv.cs
@@ -2,6 +2,7 @@ using Compze.Tests.Infrastructure;
 using Compze.Utilities.Logging;
 using Compze.Utilities.SystemCE;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Compze.Wiring;
@@ -11,6 +12,9 @@ namespace Compze.Tessaging.Hosting.Testing;
 ///<summary>TestEnvironment class. Shortened name since it is referenced statically and has nested types</summary>
 public static partial class TestEnv
 {
+   internal static IList<Func<PluggableComponents?>> _contextProviders => new List<Func<PluggableComponents?>>();
+   static PluggableComponents? GetComponentsFromProviders() => _contextProviders.Select(it => it()).NotNull().FirstOrDefault();
+
    static readonly LazyStruct<SqlLayer> SqlLayerCache = new(() =>
    {
       if(_xUnitPluggableComponentsCombination != null)
