c497cb1d2d85623b816c2dfb03333220cb8a51cf
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-12 18:11:29 +0200

    refactoring

--- src/Tests/Infrastructure/TestWiringHelper.cs
+++ src/Tests/Infrastructure/TestWiringHelper.cs
@@ -1,35 +1,11 @@
-using System;
 using Compze.Sql.DocumentDb.Abstractions;
-using Compze.Sql.DocumentDb.DependencyInjection;
-using Compze.Tessaging.Hosting.Testing.DependencyInjection;
 using Compze.Tessaging.Teventive.EventStore.Abstractions;
-using Compze.Tessaging.Teventive.EventStore.DependencyInjection;
 using Compze.Utilities.DependencyInjection.Abstractions;
-using Compze.Utilities.Logging;
-using JetBrains.Annotations;
-using static Compze.Utilities.Contracts.Assert;
 
 namespace Compze.Tests.Infrastructure;
 
 public static class TestWiringHelper
 {
-   static (Wiring.SqlLayer, Wiring.DIContainer) ParseParts(string _combination)
-   {
-      try
-      {
-         var parts = _combination.Split(':');
-
-         Argument.Is(parts.Length == 2, () => $"PluggableComponentParts has an invalid format: {_combination}");
-
-         return ((Wiring.SqlLayer)Enum.Parse(typeof(Wiring.SqlLayer), parts[0]),
-                 (Wiring.DIContainer)Enum.Parse(typeof(Wiring.DIContainer), parts[1]));
-      }
-      catch(Exception e)
-      {
-         throw new Exception($"PluggableComponentParts has an invalid format: {_combination}", e);
-      }
-   }
-
    public static IEventStore EventStore(this IServiceLocator @this) =>
       @this.Resolve<IEventStore>();
 
@@ -44,12 +20,6 @@ public static class TestWiringHelper
 
    public static IDocumentDbBulkReader DocumentDbBulkReader(this IServiceLocator @this) =>
       @this.Resolve<IDocumentDbBulkReader>();
-
-   public static IEventStoreUpdater EventStoreUpdater(this IServiceLocator @this) =>
-      @this.Resolve<IEventStoreUpdater>();
-
-   public static IEventStoreReader EventStoreReader(this IServiceLocator @this) =>
-      @this.Resolve<IEventStoreReader>();
 
    public static IDocumentDbSession DocumentDbSession(this IServiceLocator @this)
       => @this.Resolve<IDocumentDbSession>();
