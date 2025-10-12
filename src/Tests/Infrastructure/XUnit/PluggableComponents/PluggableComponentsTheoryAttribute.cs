2c881467cbf6dab3f691189ddc629931c1c1ea1c
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-12 20:26:31 +0200

    refactoring

--- src/Tests/Infrastructure/XUnit/PluggableComponents/PluggableComponentsTheoryAttribute.cs
+++ src/Tests/Infrastructure/XUnit/PluggableComponents/PluggableComponentsTheoryAttribute.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using Compze.Tessaging.Hosting.Testing;
 using Compze.Utilities.Functional;
-using Compze.Utilities.SystemCE;
 using Compze.Wiring;
 using Xunit;
-using Xunit.Sdk;
 using Xunit.v3;
 
 namespace Compze.Tests.Infrastructure.XUnit.PluggableComponents;
@@ -35,89 +29,3 @@ public sealed class PluggableComponentsTheoryAttribute(
    public Wiring.SqlLayer[]? ExcludeSqlLayers { get; init; }
 }
 #pragma warning disable CA1812 // Avoid uninstantiated internal classes : This class is instantiated by xUnit via reflection.
-class PluggableComponentsTheoryDiscoverer : IXunitTestCaseDiscoverer
-{
-#pragma warning restore CA1812 // Avoid uninstantiated internal classes : This class is instantiated by xUnit via reflection.
-   public async ValueTask<IReadOnlyCollection<IXunitTestCase>> Discover(
-      ITestFrameworkDiscoveryOptions discoveryOptions,
-      IXunitTestMethod testMethod,
-      IFactAttribute factAttribute)
-   {
-      var combinations = PluggableComponentsReader.GetCombinations();
-
-      // Filter out excluded SQL layers if specified
-      if(factAttribute is PluggableComponentsTheoryAttribute { ExcludeSqlLayers.Length: > 0 } theoryAttribute)
-      {
-         var excludedLayers = theoryAttribute.ExcludeSqlLayers;
-         combinations = combinations
-                       .Where(combo =>
-                        {
-                           var context = new PluggableComponentTestContext(combo);
-                           return !excludedLayers.Contains(TestEnv.SqlLayer);
-                        })
-                       .ToList();
-      }
-
-      var testCases = combinations
-                     .Select(combination =>
-                      {
-                         // Create and pass a PluggableComponentTestContext instance
-                         var arguments = new object[] { new PluggableComponentTestContext(combination) };
-
-                         return new PluggableComponentsTestCase(
-                            testMethod: testMethod,
-                            combination: combination,
-                            testCaseDisplayName: $"{testMethod.Method.Name}({combination})",
-                            uniqueID: $"{testMethod.UniqueID}.{combination}",
-                            @explicit: factAttribute.Explicit,
-                            timeout: factAttribute.Timeout,
-                            testMethodArguments: arguments);
-                      })
-                     .ToArray();
-
-      return await ValueTask.FromResult<IReadOnlyCollection<IXunitTestCase>>(testCases);
-   }
-}
-
-public class PluggableComponentsTestCase : XunitTestCase
-{
-   Infrastructure.PluggableComponents? _combination = null;
-
-   public Infrastructure.PluggableComponents Components => _combination!.Value;
-
-   [Obsolete("Called by deserializer")]
-   public PluggableComponentsTestCase() {}
-
-   public PluggableComponentsTestCase(
-      IXunitTestMethod testMethod,
-      Infrastructure.PluggableComponents combination,
-      string testCaseDisplayName,
-      string uniqueID,
-      bool @explicit,
-      int? timeout,
-      object?[]? testMethodArguments)
-      : base(testMethod,
-             testCaseDisplayName,
-             uniqueID,
-             @explicit,
-             skipReason: null,
-             skipType: null,
-             skipUnless: null,
-             skipWhen: null,
-             timeout: timeout,
-             testMethodArguments: testMethodArguments,
-             traits: null) =>
-      _combination = combination;
-
-   protected override void Serialize(IXunitSerializationInfo info)
-   {
-      base.Serialize(info);
-      info.AddValue(nameof(_combination), _combination.ToString());
-   }
-
-   protected override void Deserialize(IXunitSerializationInfo info)
-   {
-      base.Deserialize(info);
-      _combination = Infrastructure.PluggableComponents.FromString(info.GetValue<string>(nameof(_combination)).NotNull());
-   }
-}
---
+++ src/Tests/Infrastructure/XUnit/PluggableComponents/PluggableComponentsTheoryDiscoverer.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Compze.Tessaging.Hosting.Testing;
+using Xunit.Sdk;
+using Xunit.v3;
+
+namespace Compze.Tests.Infrastructure.XUnit.PluggableComponents;
+
+class PluggableComponentsTheoryDiscoverer : IXunitTestCaseDiscoverer
+{
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes : This class is instantiated by xUnit via reflection.
+   public async ValueTask<IReadOnlyCollection<IXunitTestCase>> Discover(
+      ITestFrameworkDiscoveryOptions discoveryOptions,
+      IXunitTestMethod testMethod,
+      IFactAttribute factAttribute)
+   {
+      var combinations = PluggableComponentsReader.GetCombinations();
+
+      // Filter out excluded SQL layers if specified
+      if(factAttribute is PluggableComponentsTheoryAttribute { ExcludeSqlLayers.Length: > 0 } theoryAttribute)
+      {
+         var excludedLayers = theoryAttribute.ExcludeSqlLayers;
+         combinations = combinations
+                       .Where(combo =>
+                        {
+                           var context = new PluggableComponentTestContext(combo);
+                           return !excludedLayers.Contains(TestEnv.SqlLayer);
+                        })
+                       .ToList();
+      }
+
+      var testCases = combinations
+                     .Select(combination =>
+                      {
+                         // Create and pass a PluggableComponentTestContext instance
+                         var arguments = new object[] { new PluggableComponentTestContext(combination) };
+
+                         return new PluggableComponentsTestCase(
+                            testMethod: testMethod,
+                            combination: combination,
+                            testCaseDisplayName: $"{testMethod.Method.Name}({combination})",
+                            uniqueID: $"{testMethod.UniqueID}.{combination}",
+                            @explicit: factAttribute.Explicit,
+                            timeout: factAttribute.Timeout,
+                            testMethodArguments: arguments);
+                      })
+                     .ToArray();
+
+      return await ValueTask.FromResult<IReadOnlyCollection<IXunitTestCase>>(testCases);
+   }
+}
