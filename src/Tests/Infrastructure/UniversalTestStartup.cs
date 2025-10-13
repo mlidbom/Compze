abeece9b3ba6946d83d7c2cfd7fbef992acbc774
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-13 13:36:34 +0200

    Cruft

--- src/Tests/Infrastructure/UniversalTestStartup.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-namespace Compze.Tests.Infrastructure;
-
-public static class UniversalTestStartup
-{
-   [ModuleInitializer]
-   public static void Initialize() => FluentAssertions.License.Accepted = true;
-}
