d1c23de31b2c87a6e732b0335b026ab23d4493a0
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-13 14:25:18 +0200

    Cruft.

--- src/Samples/AccountManagement/AccountManagementCommonAssemblyInfoInternalsVisibleToComposable.cs
+++
@@ -1,7 +0,0 @@
-
-using Composable;
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(InternalsRequiredToBeVisibleTo.Assembly1)]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(InternalsRequiredToBeVisibleTo.Assembly2)]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(InternalsRequiredToBeVisibleTo.Assembly3)]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(InternalsRequiredToBeVisibleTo.Assembly4)]
