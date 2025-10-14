e084102f782634bd95a94d5ed348eb9d8689a31c
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-14 21:55:48 +0200

    Formatting.

--- src/Compze/Utilities/Threading/TasksCE/TaskCE.Run.cs
+++ src/Compze/Utilities/Threading/TasksCE/TaskCE.Run.cs
@@ -9,7 +9,6 @@ static partial class TaskCE
    ///<summary>Like Task.Run, but this one guarantees that the task runs on a different thread, eliminating subtle and hard to debug problems in the case where TaskRun occasionally does NOT run on a different thread</summary>
    public static Task Run(Action action) => Run(action.AsUnitFunc());
 
-
    static readonly TaskFactory DefaultSchedulerDenyChildAttachTaskFactory = new(CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskContinuationOptions.None, TaskScheduler.Default);
 #pragma warning disable CA2008 // Do not create tasks without passing a TaskScheduler We just did. On the line above...
    internal static Task RunOnDedicatedThread(Action action) => DefaultSchedulerDenyChildAttachTaskFactory.StartNew(action, TaskCreationOptions.LongRunning);
