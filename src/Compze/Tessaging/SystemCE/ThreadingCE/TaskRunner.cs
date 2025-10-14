c2ffa0d92e69ea612d2dea5a5205a3b735c992db
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-14 15:25:05 +0200

    refactoring.

--- src/Compze/Tessaging/SystemCE/ThreadingCE/TaskRunner.cs
+++ src/Compze/Tessaging/SystemCE/ThreadingCE/TaskRunner.cs
@@ -1,10 +1,8 @@
 using Compze.Utilities.DependencyInjection;
 using Compze.Utilities.DependencyInjection.Abstractions;
 using Compze.Utilities.Logging;
-using JetBrains.Annotations;
 using System;
 using System.Threading;
-using System.Threading.Tasks;
 using Compze.Utilities.Threading.TasksCE;
 
 namespace Compze.Tessaging.SystemCE.ThreadingCE;
@@ -19,32 +17,32 @@ interface ITaskRunner
 static class TaskRunnerRegistrar
 {
    internal static IDependencyRegistrar TaskRunner(this IDependencyRegistrar registrar)
-      => registrar.Register(ThreadingCE.TaskRunner.RegisterWith);
-}
+      => registrar.Register(TaskRunnerImpl.RegisterWith);
 
-[UsedImplicitly] class TaskRunner : ITaskRunner, IDisposable
-{
-   internal static void RegisterWith(IDependencyRegistrar registrar)
-      => registrar.Register(Singleton.For<ITaskRunner>().CreatedBy(() => new TaskRunner()));
-
-   TaskRunner() {}
-
-   public void RunSwallowAndLogExceptions(string taskName, Action task)
+   class TaskRunnerImpl : ITaskRunner, IDisposable
    {
-      TaskCE.Run(() =>
+      internal static void RegisterWith(IDependencyRegistrar registrar)
+         => registrar.Register(Singleton.For<ITaskRunner>().CreatedBy(() => new TaskRunnerImpl()));
+
+      TaskRunnerImpl() {}
+
+      public void RunSwallowAndLogExceptions(string taskName, Action task)
       {
-         try
+         TaskCE.Run(() =>
          {
-            task();
-         }
-         catch(Exception exception)
-         {
-            this.Log().Error(exception, "Exception thrown on background thread. ");
-         }
-      });
+            try
+            {
+               task();
+            }
+            catch(Exception exception)
+            {
+               this.Log().Error(exception, "Exception thrown on background thread. ");
+            }
+         });
+      }
+
+      readonly CancellationTokenSource _cancellationTokenSource = new();
+
+      public void Dispose() => _cancellationTokenSource.Dispose();
    }
-
-   readonly CancellationTokenSource _cancellationTokenSource = new();
-
-   public void Dispose() => _cancellationTokenSource.Dispose();
 }
