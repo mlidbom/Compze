786c9dcaea004d6d4d6e873bcdc81b2e56c5a7db
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-14 15:18:29 +0200

    cruft

--- src/Compze/Utilities/Threading/TasksCE/TaskCE.Run.cs
+++ src/Compze/Utilities/Threading/TasksCE/TaskCE.Run.cs
@@ -2,15 +2,10 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-// ReSharper disable MethodSupportsCancellation
-#pragma warning disable CA2008 // Do not create tasks without passing a TaskScheduler. We use our own factory instance that already specifies the Scheduler
-
 namespace Compze.Utilities.Threading.TasksCE;
 
 static partial class TaskCE
 {
-   static readonly TaskFactory DefaultSchedulerDenyChildAttachTaskFactory = new(CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskContinuationOptions.None, TaskScheduler.Default);
-
    ///<summary>Like Task.Run, but this one guarantees that the task runs on a different thread, eliminating subtle and hard to debug problems in the case where TaskRun occasionally does NOT run on a different thread</summary>
    public static Task Run(Action action) => Run(action.AsUnitFunc());
 
