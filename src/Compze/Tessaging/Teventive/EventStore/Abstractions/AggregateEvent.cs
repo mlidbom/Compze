38643c983d9350e6df1875e9dad2dca934f93e3b
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-15 13:10:37 +0200

    commetns

--- src/Compze/Tessaging/Teventive/EventStore/Abstractions/AggregateEvent.cs
+++ src/Compze/Tessaging/Teventive/EventStore/Abstractions/AggregateEvent.cs
@@ -9,7 +9,7 @@ public abstract class AggregateEvent() : IMutableAggregateEvent
     public Guid MessageId { get; private set; } = Guid.CreateVersion7();
     public int AggregateVersion { get; private set; }
     public Guid AggregateId { get; private set; }
-    public DateTime UtcTimeStamp { get; private set; } = DateTime.UtcNow; //Todo:bug: Should use timesource.
+    public DateTime UtcTimeStamp { get; private set; } = DateTime.UtcNow; //Todo: Should use time source.
 
 #pragma warning disable CA1033 // We do not want these methods as part of the public interface of AggregateEvent.
     void IMutableAggregateEvent.SetAggregateIdInternal(Guid aggregateId) => AggregateId = aggregateId;
