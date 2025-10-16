bc1ef65c63e0462e1533095b1a283488715258b3
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-16 11:11:22 +0200

    checkpoi

--- src/Tests/Performance/Internals/XUnit/Properties/AssemblyInfo.cs
+++ src/Tests/Performance/Internals/XUnit/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
+
 //Nothing in this project should run in parallel
-//Note: For XUnit tests, the [Performance] attribute must be applied to each test class individually,
-//as XUnit v2 does not support assembly-level trait attributes.
 [assembly: NCrunch.Framework.EnableRdi(false)]
 [assembly: NCrunch.Framework.Serial]
+
+[assembly: Xunit.CollectionBehavior(DisableTestParallelization = true)]
