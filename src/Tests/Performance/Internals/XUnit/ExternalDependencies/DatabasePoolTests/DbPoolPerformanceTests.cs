e24b2ddcd285d08f40a9fab8d2afbbe80af40da6
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-16 13:17:37 +0200

    checkpoint

--- src/Tests/Performance/Internals/XUnit/ExternalDependencies/DatabasePoolTests/DbPoolPerformanceTests.cs
+++ src/Tests/Performance/Internals/XUnit/ExternalDependencies/DatabasePoolTests/DbPoolPerformanceTests.cs
@@ -21,8 +21,7 @@ public class DbPoolPerformanceTests : DbPoolTestBase
       pool.ConnectionStringFor(Guid.NewGuid().ToString());
    }
 
-   [PCT]
-   public void Single_thread_can_reserve_and_release_5_identically_named_databases_in_milliseconds_msSql_150_mySql_150_pgSql_150_orcl_300_db2_150()
+   [PCT] public void Single_thread_can_reserve_and_release_5_identically_named_databases_in_milliseconds_msSql_150_mySql_150_pgSql_150_orcl_300_db2_150()
    {
       var dbName = Guid.NewGuid().ToString();
 
@@ -38,10 +37,9 @@ public class DbPoolPerformanceTests : DbPoolTestBase
          maxTotal: TestEnv.SqlLayer.ValueFor(msSql: 150, mySql: 150, pgSql: 150, sqlite: 150).Milliseconds());
    }
 
-   [PCT]
-   public void Multiple_threads_can_reserve_and_release_5_identically_named_databases_in_milliseconds_db2_50_msSql_75_mySql_75_orcl_100_pgSql_25()
+   [PCT] public void Multiple_threads_can_reserve_and_release_5_identically_named_databases_in_milliseconds_db2_50_msSql_75_mySql_75_orcl_100_pgSql_25()
    {
-      var maxTime = TestEnv.SqlLayer.ValueFor(msSql: 75, mySql: 75, pgSql: 25, sqlite: 75).Milliseconds().EnvMultiply(instrumented:1.2);
+      var maxTime = TestEnv.SqlLayer.ValueFor(msSql: 75, mySql: 75, pgSql: 25, sqlite: 75).Milliseconds().EnvMultiply(instrumented: 1.2);
       var dbName = Guid.NewGuid().ToString();
       TimeAsserter.ExecuteThreaded(
          action:
@@ -55,10 +53,9 @@ public class DbPoolPerformanceTests : DbPoolTestBase
          maxTotal: maxTime);
    }
 
-   [PCT]
-   public void Multiple_threads_can_reserve_and_release_5_differently_named_databases_in_milliseconds_msSql_125_mySql_175_pgSql_400_orcl_400_db2_100()
+   [PCT] public void Multiple_threads_can_reserve_and_release_5_differently_named_databases_in_milliseconds_msSql_125_mySql_175_pgSql_400_orcl_400_db2_100()
    {
-      var maxTotal = TestEnv.SqlLayer.ValueFor(msSql: 70, mySql: 175, pgSql: 400, sqlite: 125).Milliseconds().EnvMultiply(instrumented:1.6);
+      var maxTotal = TestEnv.SqlLayer.ValueFor(msSql: 70, mySql: 175, pgSql: 400, sqlite: 125).Milliseconds().EnvMultiply(instrumented: 1.6);
       TimeAsserter.ExecuteThreaded(
          action: () =>
          {
@@ -70,8 +67,7 @@ public class DbPoolPerformanceTests : DbPoolTestBase
          maxTotal: maxTotal);
    }
 
-   [PCT]
-   public void Single_thread_can_reserve_and_release_5_differently_named_databases_in_milliseconds_msSql_100_mySql_100_pgSql_500_orcl_300_db2_100()
+   [PCT] public void Single_thread_can_reserve_and_release_5_differently_named_databases_in_milliseconds_msSql_100_mySql_100_pgSql_500_orcl_300_db2_100()
    {
       TimeAsserter.Execute(
          action: () =>
@@ -84,8 +80,7 @@ public class DbPoolPerformanceTests : DbPoolTestBase
          maxTotal: TestEnv.SqlLayer.ValueFor(msSql: 100, mySql: 170, pgSql: 500, sqlite: 100).Milliseconds());
    }
 
-   [PCT]
-   public void Repeated_fetching_of_same_connection_runs_20_times_in_1_milliseconds()
+   [PCT] public void Repeated_fetching_of_same_connection_runs_20_times_in_1_milliseconds()
    {
       var dbName = Guid.NewGuid().ToString();
       using var manager = CreatePool();
@@ -98,10 +93,9 @@ public class DbPoolPerformanceTests : DbPoolTestBase
          maxTotal: 1.Milliseconds());
    }
 
-   [PCT]
-   public void Once_DB_Fetched_Can_use_XX_connections_in_10_millisecond_db2_50_MsSql_180_MySql_24_Oracle_140_PgSql_300()
+   [PCT] public void Once_DB_Fetched_Can_use_XX_connections_in_10_millisecond_db2_50_MsSql_180_MySql_24_Oracle_140_PgSql_300()
    {
-      var allowedTime = 10.Milliseconds().EnvMultiply(instrumented:2);
+      var allowedTime = 10.Milliseconds().EnvMultiply(instrumented: 2);
       var iterations = TestEnv.SqlLayer.ValueFor(msSql: 180, mySql: 24, pgSql: 300, sqlite: 180);
 
       using var manager = CreatePool();
@@ -138,7 +132,7 @@ public class DbPoolPerformanceTests : DbPoolTestBase
       TimeAsserter.Execute(
          action: useConnection,
          maxTotal: allowedTime,
-         iterations : iterations
+         iterations: iterations
       );
    }
 }
