0025cf0c3030b34f275d84f4124da6f3e45e5b92
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-16 14:22:05 +0200

    checkpoint

--- src/Tests/Integration/XUnit/Tessaging/ServiceBusSpecification/Given_a_backend_endpoint_with_a_command_event_and_query_handler/Transaction_policies_internal.cs
+++ src/Tests/Integration/XUnit/Tessaging/ServiceBusSpecification/Given_a_backend_endpoint_with_a_command_event_and_query_handler/Transaction_policies_internal.cs
@@ -1,34 +1,33 @@
 using System.Threading.Tasks;
 using Compze.Tessaging.Common;
 using Compze.Tessaging.Hosting;
-using Compze.Tests.Common.NUnit.Tessaging.ServiceBusSpecification.Given_a_backend_endpoint_with_a_command_event_and_query_handler;
 using Compze.Tests.Common.Tessaging.ServiceBusSpecification.Given_a_backend_endpoint_with_a_command_event_and_query_handler;
+using Compze.Tests.Infrastructure.XUnit.PluggableComponents;
 using Compze.Utilities.SystemCE.TransactionsCE;
 using FluentAssertions;
-using NUnit.Framework;
 using static FluentAssertions.FluentActions;
 
-namespace Compze.Tests.Integration.Internals.Tessaging.ServiceBusSpecification.Given_a_backend_endpoint_with_a_command_event_and_query_handler;
+namespace Compze.Tests.Integration.XUnit.Tessaging.ServiceBusSpecification.Given_a_backend_endpoint_with_a_command_event_and_query_handler;
 
-public class Transaction_policies_internal(string pluggableComponentsCombination) : NUnitEndpointHostTestBase(pluggableComponentsCombination)
+public class Transaction_policies_internal : XUnitEndpointHostTestBase
 {
-   [Test] public async Task Calling_PostRemoteAsync_within_a_transaction_with_AtLeastOnceCommand_throws_TransactionPolicyViolationException() =>
+   [PCT] public async Task Calling_PostRemoteAsync_within_a_transaction_with_AtLeastOnceCommand_throws_TransactionPolicyViolationException() =>
       await Invoking(() => TransactionScopeCe.Execute(() => ClientEndpoint.ExecuteClientRequest(session => session.PostAsync(MyAtMostOnceCommandWithResult.Create()))))
            .Should().ThrowAsync<MessageInspector.TransactionPolicyViolationException>();
 
-   [Test] public async Task Calling_GetRemoteAsync_within_a_transaction_with_Query_throws_TransactionPolicyViolationException() =>
+   [PCT] public async Task Calling_GetRemoteAsync_within_a_transaction_with_Query_throws_TransactionPolicyViolationException() =>
       await Invoking(() => TransactionScopeCe.Execute(() => ClientEndpoint.ExecuteClientRequest(session => session.GetAsync(new MyQuery()))))
            .Should().ThrowAsync<MessageInspector.TransactionPolicyViolationException>();
 
-   [Test] public void Calling_PostRemoteAsync_within_a_transaction_AtLeastOnceCommand_throws_TransactionPolicyViolationException() =>
+   [PCT] public void Calling_PostRemoteAsync_within_a_transaction_AtLeastOnceCommand_throws_TransactionPolicyViolationException() =>
       Invoking(() => TransactionScopeCe.Execute(() => ClientEndpoint.ExecuteClientRequest(session => session.Post(MyAtMostOnceCommandWithResult.Create()))))
         .Should().Throw<MessageInspector.TransactionPolicyViolationException>();
 
-   [Test] public void Calling_PostRemoteAsync_without_a_transaction_with_ExactlyOnceCommand_throws_TransactionPolicyViolationException() =>
+   [PCT] public void Calling_PostRemoteAsync_without_a_transaction_with_ExactlyOnceCommand_throws_TransactionPolicyViolationException() =>
       Invoking(() => RemoteEndpoint.ExecuteServerRequest(session => session.Send(new MyExactlyOnceCommand())))
         .Should().Throw<MessageInspector.TransactionPolicyViolationException>();
 
-   [Test] public void Calling_GetRemote_within_a_transaction_with_Query_throws_TransactionPolicyViolationException() =>
+   [PCT] public void Calling_GetRemote_within_a_transaction_with_Query_throws_TransactionPolicyViolationException() =>
       Invoking(() => TransactionScopeCe.Execute(() => ClientEndpoint.ExecuteClientRequest(session => session.Get(new MyQuery()))))
         .Should().Throw<MessageInspector.TransactionPolicyViolationException>();
 }
