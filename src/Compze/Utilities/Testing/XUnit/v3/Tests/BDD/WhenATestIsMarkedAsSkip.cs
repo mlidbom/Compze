75236c501b7a3b895b01458195fa9f58d3b24164
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-20 21:31:11 +0200

    Test skipping.

---
+++ src/Compze/Utilities/Testing/XUnit/v3/Tests/BDD/WhenATestIsMarkedAsSkip.cs
@@ -0,0 +1,8 @@
+using Compze.Utilities.Testing.XUnit.BDD;
+
+namespace Compze.Utilities.Testing.XUnit.v3.Tests.BDD;
+
+public class WhenATestIsMarkedAsSkip
+{
+   [XF(Skip = "test skipping")] public void ItIsNotExecuted() => throw new Exception("This should have been skipped");
+}
