fa39a48d6d9ceee5f5f2811ea6af17ce820d6d35
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-20 20:59:03 +0200

    adjust timings

--- src/Tests/Performance/Internals/StrictlyManagedResource/StrictlyManagedResourcePerformanceTests.cs
+++ src/Tests/Performance/Internals/StrictlyManagedResource/StrictlyManagedResourcePerformanceTests.cs
@@ -21,11 +21,11 @@ public class StrictlyManagedResourcePerformanceTests : UniversalTestBase
    }
 #pragma warning restore CA1812 // Class is never instantiated
 
-   [XF] public void Allocated_and_disposes_250_instances_in_40_millisecond_when_actually_collecting_stack_traces()
+   [XF] public void Allocated_and_disposes_100_instances_in_100_millisecond_when_actually_collecting_stack_traces()
    {
       TimeAsserter.Execute(action: () => new StrictlyManagedResource<StrictResource>(forceStackTraceCollection: true).Dispose(),
-                           iterations: 250,
-                           maxTotal: 350.Milliseconds().EnvMultiply(unoptimized: 1.3));
+                           iterations: 100,
+                           maxTotal: 100.Milliseconds().EnvMultiply(unoptimized: 1.3));
    }
 
    [XF] public void Allocates_and_disposes_5000_instances_in_10_millisecond_when_not_collecting_stack_traces()
