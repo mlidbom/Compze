542fea76443da548aaae34636493097bbf5e00f7
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-20 11:09:44 +0200

    Checkpoint.

---
+++ src/Compze/Utilities/Testing/XUnit/Tests/ComponentPermutations/XFactTest.cs
@@ -0,0 +1,12 @@
+using Compze.Utilities.Testing.XUnit.ComponentPermutations;
+
+namespace Compze.Utilities.Testing.XUnit.Tests.ComponentPermutations;
+
+public class ComponentPermutationsTest
+{
+    [PCT]
+    public void RunsWithEachComponentCombination()
+    {
+        
+    }
+}
