eca3630658280b0619d5014ff7abf5f4054f448b
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-20 11:33:38 +0200

    checkpoint

--- src/Compze/Utilities/Testing/XUnit/Tests/ComponentPermutations/XFactTest.cs
+++ src/Compze/Utilities/Testing/XUnit/Tests/ComponentPermutations/XFactTest.cs
@@ -1,12 +1,12 @@
 using Compze.Utilities.Testing.XUnit.ComponentPermutations;
+using Xunit.Abstractions;
 
 namespace Compze.Utilities.Testing.XUnit.Tests.ComponentPermutations;
 
-public class ComponentPermutationsTest
+public class ComponentPermutationsTest(ITestOutputHelper testOutputHelper)
 {
-    [PCT]
-    public void RunsWithEachComponentCombination()
-    {
-        
-    }
+   readonly ITestOutputHelper _testOutputHelper = testOutputHelper;
+
+   [PCT] public void RunsWithEachComponentCombination() =>
+      _testOutputHelper.WriteLine(TestContext.CurrentTestCase!.TestMethodArguments[0].ToString());
 }
