05cf733b3b19277a937a5ba89134ac4e9eee8118
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-20 21:26:01 +0200

    Make sure to pass along all the information discovered so that skip etc works.

--- src/Compze/Utilities/Testing/XUnit/v3/BDD/XFactDiscoverer.cs
+++ src/Compze/Utilities/Testing/XUnit/v3/BDD/XFactDiscoverer.cs
@@ -21,11 +21,19 @@ class XFactDiscoverer : IXunitTestCaseDiscoverer
       var details = TestIntrospectionHelper.GetTestCaseDetails(discoveryOptions, testMethod, factAttribute);
 
       var testCase = new XFactTestCase(
-         details.ResolvedTestMethod,
-         details.TestCaseDisplayName,
-         details.UniqueID,
-         details.Explicit,
-         testMethod.Traits.ToReadWrite(StringComparer.OrdinalIgnoreCase)
+         testMethod: details.ResolvedTestMethod,
+         testCaseDisplayName: details.TestCaseDisplayName,
+         uniqueID: details.UniqueID,
+         @explicit: details.Explicit,
+         skipExceptions: details.SkipExceptions,
+         skipReason: details.SkipReason,
+         skipType: details.SkipType,
+         skipUnless: details.SkipUnless,
+         skipWhen: details.SkipWhen,
+         traits: testMethod.Traits.ToReadWrite(StringComparer.OrdinalIgnoreCase),
+         sourceFilePath: details.SourceFilePath,
+         sourceLineNumber: details.SourceLineNumber,
+         timeout: details.Timeout
       );
 
       return await ValueTask.FromResult<IReadOnlyCollection<IXunitTestCase>>([testCase]);
