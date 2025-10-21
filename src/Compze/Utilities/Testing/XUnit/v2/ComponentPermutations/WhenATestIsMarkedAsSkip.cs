49f68622541617befe01ebdca48a70904fa92778
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-21 18:35:41 +0200

    Remove wayward test.

--- src/Compze/Utilities/Testing/XUnit/v2/ComponentPermutations/WhenATestIsMarkedAsSkip.cs
+++
@@ -1,8 +0,0 @@
-namespace Compze.Utilities.Testing.XUnit.ComponentPermutations;
-
-public class WhenATestIsMarkedAsSkip
-{
-   public WhenATestIsMarkedAsSkip() => throw new Exception("Constructor should not be called for ignored tests");
-
-   [PCT(Skip = "test skipping")] public void ItIsNotExecuted() => throw new Exception("Ignored test methods should not be executed");
-}
