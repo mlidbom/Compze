409c58d7894c25da61b906d27f6645a6339ba325
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-24 16:58:53 +0200

    checkpoint

--- src/Compze/Tessaging/Hosting/Implementation/MessageDispatchingFailedException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Compze.Tessaging.Hosting.Implementation;
-
-public class MessageDispatchingFailedException(string remoteExceptionAsString) : Exception($"""
-                                                                                            Dispatching message failed. Remote exception message: 
-                                                                                            {remoteExceptionAsString} 
-                                                                                            """);
