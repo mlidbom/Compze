cfd05253fedaa2d76ac6861ca1c7b859a6bea618
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-25 02:27:31 +0200

    comment

--- src/Compze/Abstractions/Serialization/Internal/_Interfaces.cs
+++ src/Compze/Abstractions/Serialization/Internal/_Interfaces.cs
@@ -6,6 +6,8 @@ namespace Compze.Abstractions.Serialization.Internal;
 
 interface IJsonSerializer
 {
+   //todo: we should be using the interface, not concrete implementations, so this should not be unused.
+   // We should also inject the serializer, not instantiate it manually.
    string Serialize(object instance);
    object Deserialize(Type type, string json);
 }
