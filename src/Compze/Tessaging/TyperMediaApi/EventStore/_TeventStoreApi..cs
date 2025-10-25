f39e820be34d3179c91f7758d8fc8f4808edf012
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-25 15:46:31 +0200

    checkpoint

--- src/Compze/Tessaging/TyperMediaApi/EventStore/_TeventStoreApi..cs
+++ src/Compze/Tessaging/TyperMediaApi/EventStore/_TeventStoreApi..cs
@@ -24,6 +24,6 @@ public partial class TeventStoreApi
 
    public partial class TommandApi
    {
-      public SaveTaggregate<TTaggregate> Save<TTaggregate>(TTaggregate account) where TTaggregate : class, ITaggregate => new(account);
+      public SaveTaggregate<TTaggregate> Save<TTaggregate>(TTaggregate aggregate) where TTaggregate : class, ITaggregate => new(aggregate);
    }
 }
