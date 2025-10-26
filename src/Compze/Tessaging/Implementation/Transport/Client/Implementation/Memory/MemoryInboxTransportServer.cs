dbd793182a8f6c3a35dfc20320b64fd6f86065ef
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-26 07:45:48 +0100

    checkpoint

--- src/Compze/Tessaging/Implementation/Transport/Client/Implementation/Memory/MemoryInboxTransportServer.cs
+++ src/Compze/Tessaging/Implementation/Transport/Client/Implementation/Memory/MemoryInboxTransportServer.cs
@@ -1,10 +1,16 @@
-using System;
-using System.Threading.Tasks;
 using Compze.Core.Tessaging.Hosting.Public;
 using Compze.Core.Tessaging.Transport.Internal;
+using Compze.Tessaging.Implementation.TessageHandling.Abstractions;
+using Compze.Tessaging.Implementation.TessageHandling.Dispatching;
+using Compze.Tessaging.Implementation.TessageHandling.Inbox;
+using Compze.Tessaging.Implementation.Transport.Abstractions;
 using Compze.Utilities.Contracts;
 using Compze.Utilities.DependencyInjection;
 using Compze.Utilities.DependencyInjection.Abstractions;
+using System;
+using System.Threading.Tasks;
+using Compze.Utilities.SystemCE;
+using Compze.Utilities.Threading.TasksCE;
 
 namespace Compze.Tessaging.Implementation.Transport.Client.Implementation.Memory;
 
@@ -12,18 +18,26 @@ static class MemoryInboxTransportServerRegistrar
 {
    internal static IComponentRegistrar MemoryTransport(this IComponentRegistrar registrar) =>
       registrar.Register(Singleton.For<IInboxTransportServer, MemoryInboxTransportServer>()
-                                  .CreatedBy((EndpointId endpointId) => new MemoryInboxTransportServer(endpointId)));
+                                  .CreatedBy((EndpointId endpointId, IServiceLocator serviceLocator) => new MemoryInboxTransportServer(endpointId, serviceLocator)));
 }
 
 class MemoryInboxTransportServer : IInboxTransportServer
 {
-   internal MemoryInboxTransportServer(EndpointId endpointId) =>
+   readonly LazyCE<IInbox> _inbox;
+   readonly LazyCE<Inbox.HandlerExecutionEngine> _engine;
+
+   internal MemoryInboxTransportServer(EndpointId endpointId, IServiceLocator serviceLocator)
+   {
+      _inbox = new LazyCE<IInbox>(serviceLocator.Resolve<IInbox>);
+      _engine = new LazyCE<Inbox.HandlerExecutionEngine>(serviceLocator.Resolve<Inbox.HandlerExecutionEngine>);;
       Address = new Uri($"memory://{endpointId.GuidValue.ToString()}");
+   }
 
    public Uri Address { get; }
 
    public bool Running { get; private set; }
    public ValueTask DisposeAsync() => ValueTask.CompletedTask;
+
    public Task StartAsync()
    {
       Assert.State.Is(!Running);
@@ -35,5 +49,63 @@ class MemoryInboxTransportServer : IInboxTransportServer
    {
       Running = false;
       return Task.CompletedTask;
+   }
+
+   public async Task<TResult> PostAsync<TResult>(TransportTessage.OutGoing tessage, object realTessage, EndPointAddress endPointAddress)
+   {
+      var incomingTessage = tessage.ToIncoming();
+      try
+      {
+         if(!Running)
+            throw new Exception("Transport is not running");
+
+         switch(tessage.TessageTypeEnum)
+         {
+            case TransportTessage.TransportTessageType.AtMostOnceTommandWithReturnValue:
+               return (await _inbox.Value.Receive(incomingTessage).caf())
+                     .NotNull()
+                     .CastTo<TResult>();
+            case TransportTessage.TransportTessageType.NonTransactionalTuery:
+               return (await _engine.Value.Enqueue(incomingTessage).caf())
+                     .NotNull()
+                     .CastTo<TResult>();
+            case TransportTessage.TransportTessageType.ExactlyOnceTevent:
+            case TransportTessage.TransportTessageType.AtMostOnceTommand:
+            case TransportTessage.TransportTessageType.ExactlyOnceTommand:
+            default:
+               throw new ArgumentOutOfRangeException();
+         }
+      }
+      catch(Exception ex)
+      {
+         throw new TessageDispatchingFailedException(ex.ToString());
+      }
+   }
+
+   public async Task PostAsync(TransportTessage.OutGoing tessage, object realTessage, EndPointAddress endPointAddress)
+   {
+      var incomingTessage = tessage.ToIncoming();
+      try
+      {
+         if(!Running)
+            throw new Exception("Transport is not running");
+
+         switch(tessage.TessageTypeEnum)
+         {
+            case TransportTessage.TransportTessageType.ExactlyOnceTevent:
+            case TransportTessage.TransportTessageType.AtMostOnceTommand:
+            case TransportTessage.TransportTessageType.ExactlyOnceTommand:
+               await _inbox.Value.Receive(incomingTessage).caf();
+               return;
+            case TransportTessage.TransportTessageType.AtMostOnceTommandWithReturnValue:
+            case TransportTessage.TransportTessageType.NonTransactionalTuery:
+            default:
+               throw new ArgumentOutOfRangeException();
+         }
+      }
+      catch(Exception ex)
+      {
+         throw new TessageDispatchingFailedException(ex.ToString());
+      }
    }
 }
