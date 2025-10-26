379d547a72eda4150b349ba55686b8a563caa4ee
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-26 07:50:57 +0100

    checkpoint

--- src/Compze/Tessaging/Implementation/Transport/Client/Implementation/Memory/MemoryTransportMessagePoster.cs
+++ src/Compze/Tessaging/Implementation/Transport/Client/Implementation/Memory/MemoryTransportMessagePoster.cs
@@ -3,13 +3,8 @@ using Compze.Tessaging.Implementation.Transport.Abstractions;
 using Compze.Tessaging.Implementation.Transport.Client.Routing.Abstractions;
 using Compze.Utilities.DependencyInjection;
 using Compze.Utilities.DependencyInjection.Abstractions;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Compze.Tessaging.Implementation.TessageHandling.Abstractions;
-using Compze.Tessaging.Implementation.TessageHandling.Dispatching;
-using Compze.Tessaging.Implementation.TessageHandling.Inbox;
-using Compze.Utilities.SystemCE;
 using Compze.Utilities.Threading.TasksCE;
 
 namespace Compze.Tessaging.Implementation.Transport.Client.Implementation.Memory;
@@ -33,19 +28,21 @@ class MemoryTransportMessagePoster : ITransportMessagePoster
 
    public async Task<TResult> PostAsync<TResult>(TransportTessage.OutGoing tessage, object realTessage, EndPointAddress endPointAddress)
    {
-      return await _endpointRegistry.ServerEndpoints
-                                    .Single(it => it.Address == endPointAddress)
-                                    .ServiceLocator
-                                    .Resolve<MemoryInboxTransportServer>()
-                                    .PostAsync<TResult>(tessage, realTessage, endPointAddress).caf();
+      return await GetTransport(endPointAddress)
+                  .PostAsync<TResult>(tessage, realTessage, endPointAddress).caf();
    }
 
    public async Task PostAsync(TransportTessage.OutGoing tessage, object realTessage, EndPointAddress endPointAddress)
    {
-      await _endpointRegistry.ServerEndpoints
-                                      .Single(it => it.Address.NotNull().Uri == endPointAddress.Uri)
-                                      .ServiceLocator
-                                      .Resolve<MemoryInboxTransportServer>()
-                                      .PostAsync(tessage, realTessage, endPointAddress).caf();
+      await GetTransport(endPointAddress)
+           .PostAsync(tessage, realTessage, endPointAddress).caf();
+   }
+
+   MemoryInboxTransportServer GetTransport(EndPointAddress endPointAddress)
+   {
+      return _endpointRegistry.ServerEndpoints
+                              .Single(it => it.Address == endPointAddress)
+                              .ServiceLocator
+                              .Resolve<MemoryInboxTransportServer>();
    }
 }
