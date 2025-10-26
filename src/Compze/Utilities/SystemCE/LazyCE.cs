7b0207b18c1d22311550d2e05242b1158a992938
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-26 08:12:16 +0100

    checkpoint

--- src/Compze/Utilities/SystemCE/LazyCE.cs
+++ src/Compze/Utilities/SystemCE/LazyCE.cs
@@ -14,15 +14,3 @@ class LazyCE<TValue>(Func<TValue> factory)
 
    public TValue? ValueIfInitialized() => _value;
 }
-
-class LazyStruct<TValue>(Func<TValue> factory)
-   where TValue : struct
-{
-   readonly MonitorCE _monitor = MonitorCE.WithDefaultTimeout();
-   TValue? _value;
-   readonly Func<TValue> _factory = factory;
-
-   public TValue Value => _value ?? _monitor.Update(() => _value ??= _factory());
-
-   public TValue? ValueIfInitialized() => _value;
-}
