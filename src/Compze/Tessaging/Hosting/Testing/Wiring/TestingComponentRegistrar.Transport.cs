7f0b30f0db0faed6fc577bc08205f98f89c76e77
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-26 05:16:45 +0100

    checkpoint

--- src/Compze/Tessaging/Hosting/Testing/Wiring/TestingComponentRegistrar.Transport.cs
+++ src/Compze/Tessaging/Hosting/Testing/Wiring/TestingComponentRegistrar.Transport.cs
@@ -1,6 +1,8 @@
 using System;
 using Compze.Core.Wiring.Testing.Internal;
 using Compze.Tessaging.Hosting.AspNetCore.Wiring;
+using Compze.Tessaging.Implementation.Transport.Client.Implementation.Http;
+using Compze.Tessaging.Implementation.Transport.Client.Implementation.Memory;
 using Compze.Utilities.DependencyInjection.Abstractions;
 using Compze.Utilities.SystemCE;
 using Compze.Utilities.Testing.DbPool;
@@ -9,7 +11,7 @@ namespace Compze.Tessaging.Hosting.Testing.Wiring;
 
 public static class TestingComponentRegistrarTransport
 {
-   public static IComponentRegistrar CurrentTestsTransport(this IComponentRegistrar register) => 
+   public static IComponentRegistrar CurrentTestsTransport(this IComponentRegistrar register) =>
       register.CastTo<TestingComponentRegistrar>().CurrentTestsTransport();
 
    public static IComponentRegistrar CurrentTestsTransport(this TestingComponentRegistrar @this)
@@ -18,8 +20,11 @@ public static class TestingComponentRegistrarTransport
       switch(TestEnv.Transport)
       {
          case Transport.AspNetCore:
-            return @this.AspNetCoreTransport();
+            return @this.AspNetCoreTransport()
+                        .HttpApiTransportClient();
          case Transport.Memory:
+            return @this.MemoryTransport()
+                        .MemoryApiTransportClient();
          default:
             throw new ArgumentOutOfRangeException();
       }
--- src/Compze/Tessaging/Implementation/Transport/Client/Implementation/Memory/MemoryInboxTransportServer.cs
+++ src/Compze/Tessaging/Implementation/Transport/Client/Implementation/Memory/MemoryInboxTransportServer.cs
@@ -2,8 +2,17 @@ using System;
 using System.Threading.Tasks;
 using Compze.Core.Tessaging.Hosting.Public;
 using Compze.Core.Tessaging.Transport.Internal;
+using Compze.Utilities.DependencyInjection;
+using Compze.Utilities.DependencyInjection.Abstractions;
 
 namespace Compze.Tessaging.Implementation.Transport.Client.Implementation.Memory;
+
+static class MemoryInboxTransportServerRegistrar
+{
+   internal static IComponentRegistrar MemoryTransport(this IComponentRegistrar registrar) =>
+      registrar.Register(Singleton.For<IInboxTransportServer>()
+                                  .CreatedBy((EndpointId endpointId) => new MemoryInboxTransportServer(endpointId)));
+}
 
 class MemoryInboxTransportServer : IInboxTransportServer
 {
