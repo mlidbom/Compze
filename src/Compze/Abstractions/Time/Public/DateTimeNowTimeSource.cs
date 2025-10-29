7b17277307e0c6a0f6b4b9cb9065737a36ee6b7e
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-29 22:40:47 +0100

    checkpoint

--- src/Compze/Abstractions/Time/Public/DateTimeNowTimeSource.cs
+++ src/Compze/Abstractions/Time/Public/DateTimeNowTimeSource.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Compze.Utilities.DependencyInjection;
 using Compze.Utilities.DependencyInjection.Abstractions;
+using Compze.Utilities.SystemCE;
 
 namespace Compze.Core.Time.Public;
 
@@ -21,6 +24,35 @@ static class TimeSourceRegistrar
          return DateTimeNowTimeSource.RegisterWith(registrar);
       }
    }
+}
+
+
+public static class UtcTimeSource
+{
+   static readonly IUtcTimeTimeSource TimeSource = DateTimeNowTimeSource.Instance;
+
+   public static DateTime UtcNow => Override?.Value?.Invoke() ?? TimeSource.UtcNow;
+
+   static readonly ThreadLocal<Func<DateTime>?> Override = new();
+
+
+   public static TResult WithOverride<TResult>(Func<DateTime> theOverride, Func<TResult> action)
+   {
+      using(ScopedChange.Enter(() => Override.Value = theOverride, () => Override.Value = null))
+      {
+         return action();
+      }
+   }
+
+   public static async Task<TResult> WithOverrideAsync<TResult>(Func<DateTime> theOverride, Func<Task<TResult>> action)
+   {
+      using(ScopedChange.Enter(() => Override.Value = theOverride, () => Override.Value = null))
+      {
+         return await action();
+      }
+   }
+
+
 }
 
 ///<summary>Simply returns DateTime.Now or DateTime.UtcNow</summary>
