33dcf92fbc62fa0843e351b40b47989a99441f5b
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-30 13:27:39 +0100

    checkpoint

--- src/Compze/Abstractions/Public/ObsoleteMessage.cs
+++ src/Compze/Abstractions/Public/ObsoleteMessage.cs
@@ -5,6 +5,6 @@ public static class ObsoleteMessage
     public const string ForInternalUseOnly = @"
 This member breaks encapsulation and framework guarantees if used incorrectly.
 It is for building infrastructure code, usually within the framework itself.
-Be VERY sure you know what you are doing before you invoke this method from application code.
+Be VERY sure you know what you are doing before you use this member.
 ";
 }
