36eb49ba134de8115060e1343e2c9978c36d86bb
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-30 13:14:10 +0100

    checkpoint

--- src/Compze/Abstractions/Public/PersistentEntity.cs
+++ src/Compze/Abstractions/Public/PersistentEntity.cs
@@ -10,45 +10,20 @@ namespace Compze.Core.Public;
 [DebuggerDisplay("{GetType().Name} Id={Id}")]
 public class Entity<TEntity> : Entity<TEntity, Guid>, IEntity, IEquatable<TEntity> where TEntity : Entity<TEntity>
 {
-    /// <summary>Creates an instance using the supplied <paramref name="id"/> as the Id.</summary>
-    protected Entity(Guid id):base(new EntityId(id))
-    {
-    }
+   /// <summary>Creates an instance using the supplied <paramref name="id"/> as the Id.</summary>
+   protected Entity(Guid id) : base(new EntityId(id)) {}
 
-    /// <summary>Creates an instance using the supplied <paramref name="id"/> as the Id.</summary>
-    protected Entity(EntityId id):base(id)
-    {
-    }
+   /// <summary>Creates an instance using the supplied <paramref name="id"/> as the Id.</summary>
+   protected Entity(EntityId id) : base(id) {}
 
-    /// <summary>Creates a new instance with an automatically generated Id</summary>
-    protected Entity():base(new EntityId())
-    {
-    }
+   /// <summary>Creates a new instance with an automatically generated Id</summary>
+   protected Entity() : base(new EntityId()) {}
 
-    EntityId<Guid> IEntity<Guid>.Id => Id;
+   EntityId<Guid> IEntity<Guid>.Id => Id;
 
-    public new virtual EntityId Id
-    {
-       get => (EntityId)base.Id;
-       protected set => base.Id = value;
-    }
-
-    ///<summary>True if both instances have the same ID</summary>
-    public static bool operator ==(Entity<TEntity>? lhs, Entity<TEntity>? rhs)
-    {
-        if (ReferenceEquals(lhs, rhs))
-        {
-            return true;
-        }
-
-        return lhs is not null && lhs.Equals(rhs);
-    }
-
-    ///<summary>True if both instances do not have the same ID</summary>
-    public static bool operator !=(Entity<TEntity> lhs, Entity<TEntity> rhs) => !(lhs == rhs);
-
-    public new bool Equals(TEntity? other) => base.Equals(other);
-    // ReSharper disable once RedundantOverriddenMember If I remove this I get another worse warning...
-    public override bool Equals(object? obj) => base.Equals(obj);
-    public override int GetHashCode() => base.GetHashCode();
+   public new virtual EntityId Id
+   {
+      get => (EntityId)base.Id;
+      protected set => base.Id = value;
+   }
 }
