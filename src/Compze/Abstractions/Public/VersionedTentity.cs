7e1ee3bc4b63cda6ca56e70e9092881d150d96b3
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-30 14:28:12 +0100

    checkpoint

--- src/Compze/Abstractions/Public/VersionedTentity.cs
+++ src/Compze/Abstractions/Public/VersionedTentity.cs
@@ -5,15 +5,7 @@ namespace Compze.Core.Public;
 ///<summary>Base class for persistent entities with versioning information</summary>
 public class VersionedEntity<T> : Entity<T> where T : VersionedEntity<T>
 {
-   /// <summary>Creates an instance using the supplied <paramref name="id"/> as the Id.</summary>
-   protected VersionedEntity(Guid id):base(new EntityId(id))
-   {
-   }
-
-   protected VersionedEntity(TentityId id):base(id)
-   {
-   }
-
+   protected VersionedEntity(TentityId id) : base(id) {}
 
    ///<summary>Contains the current version of the entity</summary>
    public virtual int Version { get; protected set; }
