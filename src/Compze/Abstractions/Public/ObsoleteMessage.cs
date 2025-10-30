8785ecf98b26276c6d96ed03fc8ad1d05994b194
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-30 13:26:59 +0100

    checkpoint

--- src/Compze/Abstractions/Public/ObsoleteMessage.cs
+++ src/Compze/Abstractions/Public/ObsoleteMessage.cs
@@ -3,7 +3,7 @@ namespace Compze.Core.Public;
 public static class ObsoleteMessage
 {
     public const string ForInternalUseOnly = @"
-This member breaks encapsulation. 
+This member breaks encapsulation and framework guarantees if used incorrectly.
 It is for building infrastructure code, usually within the framework itself.
 Be VERY sure you know what you are doing before you invoke this method from application code.
 ";
