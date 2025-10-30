e7162181277696c1d836cc5bfe1ab919a183354b
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-30 13:26:40 +0100

    checkpoint

--- src/Compze/Abstractions/Public/ObsoleteMessage.cs
+++ src/Compze/Abstractions/Public/ObsoleteMessage.cs
@@ -3,8 +3,7 @@ namespace Compze.Core.Public;
 public static class ObsoleteMessage
 {
     public const string ForInternalUseOnly = @"
-This methods breaks encapsulation. 
-The interface it is in should always be implemented using explicit interface implementation so that it is hidden from normal use of instances.
+This member breaks encapsulation. 
 It is for building infrastructure code, usually within the framework itself.
 Be VERY sure you know what you are doing before you invoke this method from application code.
 ";
