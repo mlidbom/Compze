aa869472ee50dd30881d561eca64550b6f65fce0
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-10-31 05:21:36 +0100

    Restore the converter class, but don't plug it in.

--- src/Compze/Serialization/Newtonsoft/Private/PrimitiveWrappers/ValueWrapperConverter.cs
+++ src/Compze/Serialization/Newtonsoft/Private/PrimitiveWrappers/ValueWrapperConverter.cs
@@ -1,70 +1,71 @@
-//using System;
-//using System.Collections.Concurrent;
-//using Compze.Core.Public.Infrastructure;
-//using Compze.Utilities.SystemCE;
-//using Compze.Utilities.SystemCE.ReflectionCE;
-//using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using Compze.Core.Public.Infrastructure;
+using Compze.Utilities.SystemCE;
+using Compze.Utilities.SystemCE.ReflectionCE;
+using Newtonsoft.Json;
 
-//namespace Compze.Serialization.Newtonsoft.Private.PrimitiveWrappers;
+namespace Compze.Serialization.Newtonsoft.Private.PrimitiveWrappers;
 
-//public class ValueWrapperConverter : JsonConverter
-//{
-//   class WrappedTypeHelpers
-//   {
-//      public WrappedTypeHelpers(Type wrappedType, Func<object, object> construct)
-//      {
-//         WrappedType = wrappedType;
-//         Construct = construct;
-//      }
+public class ValueWrapperConverter : JsonConverter
+{
+    class WrappedTypeHelpers
+    {
+        public WrappedTypeHelpers(Type wrappedType, Func<object, object> construct)
+        {
+            WrappedType = wrappedType;
+            Construct = construct;
+        }
 
-//      public Type WrappedType { get; init; }
-//      public Func<object, object> Construct { get; init; }
-//   }
+        public Type WrappedType { get; init; }
+        public Func<object, object> Construct { get; init; }
+    }
 
-//   readonly ConcurrentDictionary<Type, bool> _handlesType = new();
-//   readonly ConcurrentDictionary<Type, LazyCE<WrappedTypeHelpers>> _wrappedTypeHelpers = new();
+    readonly ConcurrentDictionary<Type, bool> _handlesType = new();
+    readonly ConcurrentDictionary<Type, LazyCE<WrappedTypeHelpers>> _wrappedTypeHelpers = new();
 
-//   public override bool CanConvert(Type objectType) =>
-//      _handlesType.GetOrAdd(objectType,
-//                            potentialWrapperType =>
-//                               !potentialWrapperType.IsAbstract &&
-//                               potentialWrapperType.InHerits(typeof(ValueWrapper<>)));
+    public override bool CanConvert(Type objectType) =>
+       _handlesType.GetOrAdd(objectType,
+                             potentialWrapperType =>
+                                !potentialWrapperType.IsAbstract &&
+                                potentialWrapperType.InHerits(typeof(ValueWrapper<>)));
 
-//   public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
-//   {
-//      if(value == null)
-//      {
-//         //todo: ncrunch claims we never hit this line, but the DbPool serializes null DbPoolDatabase.ReservedByPoolId all the time. What's up with that?
-//         writer.WriteNull();
-//      } else
-//      {
-//         serializer.Serialize(writer, value.CastTo<ISingleUntypedPrimitiveValueWrapper>().UntypedPrimitiveValue);
-//      }
-//   }
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        if (value == null)
+        {
+            //todo: ncrunch claims we never hit this line, but the DbPool serializes null DbPoolDatabase.ReservedByPoolId all the time. What's up with that?
+            writer.WriteNull();
+        }
+        else
+        {
+            serializer.Serialize(writer, value.CastTo<ISingleUntypedPrimitiveValueWrapper>().UntypedPrimitiveValue);
+        }
+    }
 
-//   public override object? ReadJson(JsonReader reader,
-//                                    Type objectType,
-//                                    object? existingValue,
-//                                    JsonSerializer serializer)
-//   {
-//      if(reader.TokenType == JsonToken.Null)
-//         return null;
+    public override object? ReadJson(JsonReader reader,
+                                     Type objectType,
+                                     object? existingValue,
+                                     JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.Null)
+            return null;
 
-//      var helpers = _wrappedTypeHelpers.GetOrAdd(objectType,
-//                                                 wrapperType =>
-//                                                 {
-//                                                    return new LazyCE<WrappedTypeHelpers>(() =>
-//                                                    {
-//                                                       var wrappedPrimitiveType = wrapperType.GetGenericBaseClass(typeof(ValueWrapper<>))
-//                                                                                             .GetGenericArguments()[0];
-//                                                       var constructor = Constructor.Compile.ForType(objectType)
-//                                                                                            .WithArgument(wrappedPrimitiveType);
+        var helpers = _wrappedTypeHelpers.GetOrAdd(objectType,
+                                                   wrapperType =>
+                                                   {
+                                                       return new LazyCE<WrappedTypeHelpers>(() =>
+                                                     {
+                                                          var wrappedPrimitiveType = wrapperType.GetGenericBaseClass(typeof(ValueWrapper<>))
+                                                                                              .GetGenericArguments()[0];
+                                                          var constructor = Constructor.Compile.ForType(objectType)
+                                                                                             .WithArgument(wrappedPrimitiveType);
 
-//                                                       return new WrappedTypeHelpers(wrappedPrimitiveType, constructor);
-//                                                    });
-//                                                 }).Value;
+                                                          return new WrappedTypeHelpers(wrappedPrimitiveType, constructor);
+                                                      });
+                                                   }).Value;
 
-//      var primitiveValue = serializer.Deserialize(reader, helpers.WrappedType);
-//      return helpers.Construct(primitiveValue.NotNull());
-//   }
-//}
+        var primitiveValue = serializer.Deserialize(reader, helpers.WrappedType);
+        return helpers.Construct(primitiveValue.NotNull());
+    }
+}
