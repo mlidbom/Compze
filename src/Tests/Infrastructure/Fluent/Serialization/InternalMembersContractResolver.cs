ce04f3688f12e7a02c97487f8a87f5b49f848028
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-01 07:56:52 +0100

    checkpoint

---
+++ src/Tests/Infrastructure/Fluent/Serialization/InternalMembersContractResolver.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+namespace Compze.Tests.Infrastructure.Fluent.Serialization;
+
+class InternalMembersContractResolver : MemberFilteringContractResolver
+{
+   protected override bool ShouldIncludeProperty(PropertyInfo property) =>
+      property.GetMethod is { IsAssembly: true } or { IsFamilyOrAssembly: true } or { IsFamily: true };
+
+   protected override bool ShouldIncludeField(FieldInfo field) =>
+      field.IsAssembly || field.IsFamilyOrAssembly || field.IsFamily;
+}
