5c9d432c843dfc1f8ed069eb73af9b07dd0f3fc4
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-01 11:01:52 +0100

    checkpoint

--- src/Tests/Unit/Internals/Serialization/WhenSerializingPolymorphicWrapperObjects.cs
+++ src/Tests/Unit/Internals/Serialization/WhenSerializingPolymorphicWrapperObjects.cs
@@ -28,27 +28,27 @@ public class When_serializing_polymorphic_wrapper_objects : SerializerTest
                                                  "Ids": [
                                                    null,
                                                    {
-                                                     "Value": "34db64a2-dc5b-46a7-badb-d92c2e4fa0f8"
+                                                     "Value": "10000000-0000-0000-0000-000000000000"
                                                    },
                                                    {
                                                      "$type": "a1d63763-f934-493b-ae92-aeb2f15368b7",
-                                                     "Value": "780b941a-e121-49b3-b665-0d53ea6a943a"
+                                                     "Value": "20000000-0000-0000-0000-000000000000"
                                                    },
                                                    {
                                                      "$type": "5d87bfa3-5f88-4d3b-8971-c994757286ce",
-                                                     "Value": "c7c9a76d-204b-4884-acde-e7b0494e9c34"
+                                                     "Value": "30000000-0000-0000-0000-000000000000"
                                                    },
                                                    {
                                                      "$type": "0469d68b-776e-4844-8766-1cec0a563e9c",
-                                                     "Value": "23306e93-a8fe-4040-964a-d5d4a2ba119f"
+                                                     "Value": "40000000-0000-0000-0000-000000000000"
                                                    },
                                                    {
                                                      "$type": "27226150-359e-45c7-9881-35879724612b",
-                                                     "Value": "2e9f6ca8-b573-4195-8d1b-0a06c07f73a0"
+                                                     "Value": "50000000-0000-0000-0000-000000000000"
                                                    },
                                                    {
                                                      "$type": "2acb8a85-b9ba-4354-b545-9c6e86c4ece8",
-                                                     "Value": "3e633b05-8241-4257-bcbb-dd02b87a67ee"
+                                                     "Value": "60000000-0000-0000-0000-000000000000"
                                                    }
                                                  ]
                                                }
@@ -65,13 +65,13 @@ public class When_serializing_polymorphic_wrapper_objects : SerializerTest
             [PCTSerializer]
             public void is_identical_to_the_original_serialized_data() =>
                Root.Create()
-                 ._(DocumentSerializer.Serialize)
-                 ._(DocumentSerializer.Deserialize<Root>)
-                 ._(DocumentSerializer.Serialize)
-                 .Must()
-                 .Be(ExpectedSerializedData);
+                   ._(DocumentSerializer.Serialize)
+                   ._(DocumentSerializer.Deserialize<Root>)
+                   ._(DocumentSerializer.Serialize)
+                   .Must()
+                   .Be(ExpectedSerializedData);
          }
-        }
+      }
 
       [PCTSerializer] public void RoundTrippedObjectIsIdenticalToOriginal()
       {
@@ -103,12 +103,12 @@ public class When_serializing_polymorphic_wrapper_objects : SerializerTest
                       Ids = new List<EntityId?>
                             {
                                null,
-                               new EntityId(Guid.Parse("34DB64A2-DC5B-46A7-BADB-D92C2E4FA0F8")),
-                               new TentityId(Guid.Parse("780B941A-E121-49B3-B665-0D53EA6A943A")),
-                               new TaggregateId(Guid.Parse("C7C9A76D-204B-4884-ACDE-E7B0494E9C34")),
-                               new TessageId(Guid.Parse("23306E93-A8FE-4040-964A-D5D4A2BA119F")),
-                               new PersonId(Guid.Parse("2E9F6CA8-B573-4195-8D1B-0A06C07F73A0")),
-                               new UserId(Guid.Parse("3E633B05-8241-4257-BCBB-DD02B87A67EE"))
+                               new EntityId(Guid.Parse("10000000-0000-0000-0000-000000000000")),
+                               new TentityId(Guid.Parse("20000000-0000-0000-0000-000000000000")),
+                               new TaggregateId(Guid.Parse("30000000-0000-0000-0000-000000000000")),
+                               new TessageId(Guid.Parse("40000000-0000-0000-0000-000000000000")),
+                               new PersonId(Guid.Parse("50000000-0000-0000-0000-000000000000")),
+                               new UserId(Guid.Parse("60000000-0000-0000-0000-000000000000"))
                             }
                    };
          }
