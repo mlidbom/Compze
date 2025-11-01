de74efd60b868cd99cd739dcb2495b96ba4208be
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-01 02:21:33 +0100

    checkpoint

--- src/Tests/Infrastructure/Fluent/_Object_BeEquivalentTo.cs
+++ src/Tests/Infrastructure/Fluent/_Object_BeEquivalentTo.cs
@@ -9,25 +9,28 @@ namespace Compze.Tests.Infrastructure.Fluent;
 
 public static class ObjectBeEquivalentTo
 {
-   public static IMust<TValue> BeEquivalentTo<TValue>(this IMust<TValue> must, 
-                                                       TValue expected,
-                                                       [CallerArgumentExpression(nameof(expected))] string expectedExpression = null!)
+   public static IMust<TValue> BeEquivalentTo<TValue>(this IMust<TValue> must,
+                                                      TValue expected,
+                                                      [CallerArgumentExpression(nameof(expected))]
+                                                      string expectedExpression = null!)
       => BeEquivalentToCore(must, expected, expectedExpression, TestingJsonSettings.All);
 
-   public static IMust<TValue> BeEquivalentToInternal<TValue>(this IMust<TValue> must, 
-                                                               TValue expected,
-                                                               [CallerArgumentExpression(nameof(expected))] string expectedExpression = null!)
+   public static IMust<TValue> BeEquivalentToInternal<TValue>(this IMust<TValue> must,
+                                                              TValue expected,
+                                                              [CallerArgumentExpression(nameof(expected))]
+                                                              string expectedExpression = null!)
       => BeEquivalentToCore(must, expected, expectedExpression, TestingJsonSettings.Internal);
 
-   public static IMust<TValue> BeEquivalentToPublic<TValue>(this IMust<TValue> must, 
-                                                             TValue expected,
-                                                             [CallerArgumentExpression(nameof(expected))] string expectedExpression = null!)
+   public static IMust<TValue> BeEquivalentToPublic<TValue>(this IMust<TValue> must,
+                                                            TValue expected,
+                                                            [CallerArgumentExpression(nameof(expected))]
+                                                            string expectedExpression = null!)
       => BeEquivalentToCore(must, expected, expectedExpression, TestingJsonSettings.Public);
 
-   static IMust<TValue> BeEquivalentToCore<TValue>(IMust<TValue> must, 
-                                                    TValue expected, 
-                                                    string expectedExpression,
-                                                    JsonSerializerSettings settings)
+   static IMust<TValue> BeEquivalentToCore<TValue>(IMust<TValue> must,
+                                                   TValue expected,
+                                                   string expectedExpression,
+                                                   JsonSerializerSettings settings)
    {
       var actualJson = JsonConvert.SerializeObject(must.Actual, settings);
       var expectedJson = JsonConvert.SerializeObject(expected, settings);
@@ -44,17 +47,17 @@ public static class ObjectBeEquivalentTo
                               {must.Separator}
                               {expectedExpression.Indent()}
                               {must.Separator}
-                              
+
                               Actual.ToString():
                               {must.Separator}
                               {(must.Actual?.ToString() ?? "null").Indent()}
                               {must.Separator}
-                              
+
                               Expected.ToString():
                               {must.Separator}
                               {(expected?.ToString() ?? "null").Indent()}
                               {must.Separator}
-                              
+
                               JSON Diff:
                               {must.Separator}
                               {JsonDiffGenerator.CreateDiff(expectedJson, actualJson)}
