4f08854d6e1c7ea3adf00a7f264721fe44466474
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-01 02:40:27 +0100

    checkpoint

--- src/Tests/Infrastructure/Fluent/__Satisfy.cs
+++ src/Tests/Infrastructure/Fluent/__Satisfy.cs
@@ -5,15 +5,17 @@ namespace Compze.Tests.Infrastructure.Fluent;
 
 public static class _Satisfy
 {
-   public static IMust<T> Satisfy<T>(this IMust<T> context, 
-                                      Func<T, bool> predicate, 
-                                      Func<string>? messageOverride = null,
-                                      [CallerArgumentExpression(nameof(predicate))] string predicateExpression = null!)
+   public static IMust<T> Satisfy<T>(this IMust<T> context,
+                                     Func<T, bool> predicate,
+                                     Func<string>? messageOverride = null,
+                                     [CallerArgumentExpression(nameof(predicate))]
+                                     string predicateExpression = null!)
    {
       if(!predicate(context.Actual))
       {
          throw new AssertionFailedException(messageOverride?.Invoke() ?? $"{context.Expression} failed: {predicateExpression}");
       }
+
       return context;
    }
 }
