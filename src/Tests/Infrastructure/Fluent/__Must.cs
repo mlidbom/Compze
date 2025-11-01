be8e1af80656d9117f1f5dee6aab8777ede95a85
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-01 04:04:30 +0100

    checkpoint

--- src/Tests/Infrastructure/Fluent/__Must.cs
+++ src/Tests/Infrastructure/Fluent/__Must.cs
@@ -7,4 +7,7 @@ public static class __Must
    public static IMust<T> Must<T>(this T subject,
                                   [CallerArgumentExpression(nameof(subject))]
                                   string expression = null!) => new Must<T>(subject, expression);
+
+   ///<summary>Allows for more correct sentences if one does not like just chaining the assertion methods themselves</summary>
+   public static IMust<T> And<T>(this IMust<T> @this) => @this;
 }
