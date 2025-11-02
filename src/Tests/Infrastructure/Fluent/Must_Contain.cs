71e198f12ad8ad8b7f83d6eb7c4b37f9b8cf0113
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-02 19:31:55 +0100

    checkpoint

--- src/Tests/Infrastructure/Fluent/Must_Contain.cs
+++ src/Tests/Infrastructure/Fluent/Must_Contain.cs
@@ -10,6 +10,12 @@ namespace Compze.Tests.Infrastructure.Fluent;
 
 public static class Must_Contain
 {
+   public static IMust<IReadOnlySet<T>> Contain<T>(this IMust<IReadOnlySet<T>> must, T item, [CallerArgumentExpression(nameof(item))] string itemExpression = null!)
+      => must.Satisfy(it => it.Contains(item), usedArguments: [new(nameof(item), itemExpression, item)]);
+
+   public static IMust<ISet<T>> Contain<T>(this IMust<ISet<T>> must, T item, [CallerArgumentExpression(nameof(item))] string itemExpression = null!)
+      => must.Satisfy(it => it.Contains(item), usedArguments: [new(nameof(item), itemExpression, item)]);
+
     public static IMust<HashSet<T>> Contain<T>(this IMust<HashSet<T>> must, T item, [CallerArgumentExpression(nameof(item))] string itemExpression = null!)
        => must.Satisfy(it => it.Contains(item), usedArguments: [new(nameof(item), itemExpression, item)]);
 
