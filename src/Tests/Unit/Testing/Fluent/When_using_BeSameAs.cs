d65389b60f61c6c2cfb6eced3f289c3f01640514
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-02 07:50:56 +0100

    checkpoint

--- src/Tests/Unit/Testing/Fluent/When_using_BeSameAs.cs
+++ src/Tests/Unit/Testing/Fluent/When_using_BeSameAs.cs
@@ -13,11 +13,12 @@ public class When_using_BeSameAs : UniversalTestBase
 {
    public class given_two_different_objects_with_the_same_value : When_using_BeSameAs
    {
-      [XF] public void NotBeSameAs_throws_AssertionFailedException() => Invoking(() => new ValueWrapper<int>(42)
-                                                                                      .Must()
-                                                                                      .BeSameAs(new ValueWrapper<int>(42)))
-                                                                       .Must()
-                                                                       .Throw<AssertionFailedException>();
+      [XF] public void NotBeSameAs_throws_AssertionFailedException() =>
+         Invoking(() => new ValueWrapper<int>(42)
+                       .Must()
+                       .BeSameAs(new ValueWrapper<int>(42)))
+           .Must()
+           .Throw<AssertionFailedException>();
    }
 
    public class given_two_references_to_the_same_object : When_using_BeSameAs
