2f2eb9dc572064806f0abf64d92439b878340589
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-02 19:20:33 +0100

    checkpoint

--- src/Tests/Infrastructure/Fluent/Must_Contain.cs
+++ src/Tests/Infrastructure/Fluent/Must_Contain.cs
@@ -16,9 +16,9 @@ public static class Must_Contain
    //Todo: write tests
    public static IMust<TCollection> Contain<TCollection, TItem>(this IMust<TCollection> must, TItem item, [CallerArgumentExpression(nameof(item))] string itemExpression = null!)
       where TCollection : IEnumerable<TItem>
-      => must.Satisfy(it => !it.Contains(item), usedArguments: [new(nameof(item), itemExpression, item)]);
+      => must.Satisfy(it => it.Contains(item), usedArguments: [new(nameof(item), itemExpression, item)]);
 
    public static IMust<ReadOnlyCollection<TItem>> Contain<TItem>(this IMust<ReadOnlyCollection<TItem>> must, TItem item, [CallerArgumentExpression(nameof(item))] string itemExpression = null!)
-      => must.Satisfy(it => !it.Contains(item), usedArguments: [new(nameof(item), itemExpression, item)]);
+      => must.Satisfy(it => it.Contains(item), usedArguments: [new(nameof(item), itemExpression, item)]);
 
 }
