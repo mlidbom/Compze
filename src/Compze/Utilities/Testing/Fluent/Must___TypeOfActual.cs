c3daeb757055c0715745868ecad51ed21a21c660
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-03 02:00:52 +0100

    checkpoint

--- src/Compze/Utilities/Testing/Fluent/Must___TypeOfActual.cs
+++ src/Compze/Utilities/Testing/Fluent/Must___TypeOfActual.cs
@@ -5,7 +5,7 @@ public static class Must___TypeOfActual
 {
    //todo: rename
    public static IMust<TExpected> BeOfType<TExpected>(this IMust must) =>
-      must.SatisfyInternal(it => it.GetType() == typeof(TExpected))
+      must.SatisfyInternal(it => it.GetType() == typeof(TExpected), callerName: CallName.For<TExpected>())
           .Cast<TExpected>();
 
    public static IMust<TExpected> BeAssignableTo<TExpected>(this IMust must) =>
