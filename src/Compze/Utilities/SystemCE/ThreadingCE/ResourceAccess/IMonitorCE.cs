481ae0d56649cd97370b812873e944bdf2702752
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-06 02:57:13 +0100

    checkpoint

--- src/Compze/Utilities/SystemCE/ThreadingCE/ResourceAccess/IMonitorCE.cs
+++ src/Compze/Utilities/SystemCE/ThreadingCE/ResourceAccess/IMonitorCE.cs
@@ -4,6 +4,7 @@ namespace Compze.Utilities.SystemCE.ThreadingCE.ResourceAccess;
 
 interface IMonitorCE
 {
+   //the core, the functionality implemented by the actual MonitorCE implementation, upon which everything else is convenience extensions.
    TimeSpan Timeout { get; }
    IDisposable? TryTakeUpdateLockWhen(TimeSpan timeout, Func<bool> condition);
    IDisposable? TryTakeReadLockWhen(TimeSpan timeout, Func<bool> condition);
