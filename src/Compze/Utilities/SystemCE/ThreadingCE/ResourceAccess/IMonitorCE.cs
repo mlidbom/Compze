2f05385f1f2cc46260c6ee0d12745c84fe9fc503
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-06 02:55:48 +0100

    checkpoint

--- src/Compze/Utilities/SystemCE/ThreadingCE/ResourceAccess/IMonitorCE.cs
+++ src/Compze/Utilities/SystemCE/ThreadingCE/ResourceAccess/IMonitorCE.cs
@@ -4,9 +4,9 @@ namespace Compze.Utilities.SystemCE.ThreadingCE.ResourceAccess;
 
 interface IMonitorCE
 {
+   TimeSpan Timeout { get; }
    IDisposable? TryTakeUpdateLockWhen(TimeSpan timeout, Func<bool> condition);
    IDisposable? TryTakeReadLockWhen(TimeSpan timeout, Func<bool> condition);
    IDisposable? TryTakeUpdateLock(TimeSpan timeout);
    IDisposable? TryTakeReadLock(TimeSpan timeout);
-   TimeSpan Timeout { get; }
 }
