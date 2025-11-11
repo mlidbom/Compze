a68287e72ca4c984c67ab6213cfe2071c48dbcb4
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-11 14:42:52 +0100

    checkponit

---
+++ src/Tests/Unit/CQRS/Taggregates/CompositeTaggregates/IntegerId/Domain/IRootTevent.cs
@@ -0,0 +1,89 @@
+using Compze.Core.Tessaging.Teventive.Public.Taggregates.Tevents.Public;
+using JetBrains.Annotations;
+
+// ReSharper disable MemberHidesStaticFromOuterClass
+// ReSharper disable RedundantNameQualifier
+// ReSharper disable InconsistentNaming
+namespace Compze.Tests.Unit.CQRS.Taggregates.CompositeTaggregates.IntegerId.Domain;
+
+interface IRootTevent : ITaggregateTevent
+{
+   interface Created : ITaggregateCreatedTevent, PropertyUpdated.Name;
+
+   public interface PropertyUpdated
+   {
+      public interface Name : RootTevent.IRoot
+      {
+         string Name { get; }
+      }
+   }
+
+   public interface Component : IRootTevent
+   {
+      interface Renamed : PropertyUpdated.Name;
+
+      public interface PropertyUpdated : Component
+      {
+         public interface Name : PropertyUpdated
+         {
+            string Name { get; }
+         }
+      }
+
+      public interface Entity : Component
+      {
+         int EntityId { get; }
+
+         public interface Created : PropertyUpdated.Name;
+
+         interface Renamed : PropertyUpdated.Name;
+
+         public interface Removed : Entity;
+
+         public interface PropertyUpdated : Entity
+         {
+            public interface Name : PropertyUpdated
+            {
+               string Name { get; }
+            }
+         }
+      }
+   }
+
+   public interface Entity : IRootTevent
+   {
+      int EntityId { get; }
+
+      internal interface Created : PropertyUpdated.Name;
+
+      interface Renamed : PropertyUpdated.Name;
+
+      internal interface Removed : Entity;
+
+      internal interface PropertyUpdated : Entity
+      {
+         public interface Name : PropertyUpdated
+         {
+            string Name { get; }
+         }
+      }
+
+      public interface NestedEntity : Entity
+      {
+         int NestedEntityId { get; }
+
+         public interface Created : PropertyUpdated.Name;
+
+         interface Renamed : PropertyUpdated.Name;
+         public interface Removed : NestedEntity;
+
+         public interface PropertyUpdated : NestedEntity
+         {
+            public interface Name : PropertyUpdated
+            {
+               string Name { get; }
+            }
+         }
+      }
+   }
+}
