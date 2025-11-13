9551076c113b1447722945fd5e3344680b09e157
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-11-13 08:29:41 +0100

    setup basic inheritance hierarchy

--- src/Tests/Unit/CQRS/Taggregates/InheritingTaggregate/AnimalTaggregate.cs
+++ src/Tests/Unit/CQRS/Taggregates/InheritingTaggregate/AnimalTaggregate.cs
@@ -1,4 +1,5 @@
 using System;
+using Compze.Core.Public;
 using Compze.Core.Tessaging.Teventive.Public.Taggregates.BaseClasses.Public;
 using Compze.Core.Tessaging.Teventive.Public.Taggregates.Tevents.Public;
 
@@ -7,27 +8,76 @@ namespace Compze.Tests.Unit.CQRS.Taggregates.InheritingTaggregate;
 class AnimalTaggregate : Taggregate<AnimalTaggregate, IAnimalTevent, AnimalTevent, IAnimalTevent<IAnimalTevent>, AnimalTevent<AnimalTevent>>
 {
    protected override Type WrapperTEventImplementation => typeof(AnimalTevent<AnimalTevent>);
+
+   protected AnimalTaggregate()
+   {
+      RegisterTeventAppliers()
+        .For<IAnimalTevent.Born>(tevent => TimeOfBirth = tevent.UtcTimeStamp);
+    }
+
+   public DateTime TimeOfBirth { get; private set; }
+
+   protected void RegisterBirthProtected() => Publish(new AnimalTevent.Born(new TaggregateId()));
 }
 
 class CatTaggregate : AnimalTaggregate
 {
+   public CatTaggregate(){}
    protected override Type WrapperTEventImplementation => typeof(CatTevent<CatTevent>);
+
+   public static CatTaggregate RegisterBirth()
+   {
+      var cat = new CatTaggregate();
+      cat.RegisterBirthProtected();
+      return cat;
+   }
+}
+
+class DogTaggregate : AnimalTaggregate
+{
+   public DogTaggregate(){}
+   protected override Type WrapperTEventImplementation => typeof(DogTevent<DogTevent>);
+
+   public static DogTaggregate RegisterBirth()
+   {
+      var dog = new DogTaggregate();
+      dog.RegisterBirthProtected();
+      return dog;
+   }
 }
 
 
 public interface IAnimalTevent<out T> : ITaggregateIdentifyingTevent<T> where T : IAnimalTevent {}
-public interface IAnimalTevent : ITaggregateTevent {}
-public class AnimalTevent<T>(T tevent) : TaggregateIdentifyingTevent<T>(tevent), IAnimalTevent<T> where T : IAnimalTevent {}
-public class AnimalTevent : TaggregateTevent, IAnimalTevent {}
+
+public interface IAnimalTevent : ITaggregateTevent
+{
+   interface Born : IAnimalTevent, ITaggregateCreatedTevent{}
+}
+
+public class AnimalTevent<T>(T tevent) : TaggregateIdentifyingTevent<T>(tevent), IAnimalTevent<T> where T : IAnimalTevent{}
+
+public class AnimalTevent : TaggregateTevent, IAnimalTevent
+{
+   protected AnimalTevent(){}
+   protected AnimalTevent(TaggregateId taggregateId):base(taggregateId){ }
+
+    public class Born : AnimalTevent, IAnimalTevent.Born
+   {
+      public Born(TaggregateId taggregateId) : base(taggregateId){}
+   }
+}
 
 
 public interface ICatTevent<out T> : IAnimalTevent<T> where T : IAnimalTevent { }
-public interface ICatTevent : IAnimalTevent
-{
-}
-
+public interface ICatTevent : IAnimalTevent {}
 public class CatTevent<T>(T tevent) : AnimalTevent<T>(tevent) where T : IAnimalTevent {}
 public class CatTevent : AnimalTevent, ICatTevent{}
+
+
+public interface IDogTevent<out T> : IAnimalTevent<T> where T : IAnimalTevent { }
+public interface IDogTevent : IAnimalTevent {}
+public class DogTevent<T>(T tevent) : AnimalTevent<T>(tevent) where T : IAnimalTevent {}
+public class DogTevent : AnimalTevent, IDogTevent{}
 
 
 
---
+++ src/Tests/Unit/CQRS/Taggregates/InheritingTaggregate/Given_a_cat_taggregate_inheriting_from_an_animal_taggregate.cs
@@ -0,0 +1,16 @@
+using Compze.Utilities.Testing.XUnit.BDD;
+
+namespace Compze.Tests.Unit.CQRS.Taggregates.InheritingTaggregate;
+
+public class Given_a_cat_taggregate_inheriting_from_an_animal_taggregate
+{
+   [XF] public void cat_birth()
+   {
+      var cat = CatTaggregate.RegisterBirth();
+   }
+
+   [XF] public void dog_birth()
+   {
+      var cat = DogTaggregate.RegisterBirth();
+   }
+}
