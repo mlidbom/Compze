176374f28e9d1ed494ea70a4bad1153cc94c4e3d
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-21 07:18:11 +0100

    Switch to FlexRef references.

--- src/Compze.Build.InternalizedSourceReferences/test/Compze.Build.InternalizedSourceReferences.Tests/input_source/obj/Debug/net9.0/GeneratedAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// This file exists to verify that the rewriter skips obj/ directories.
-namespace SomeProject;
-
-public class GeneratedAssemblyInfo
-{
-   public const string Version = "1.0.0";
-}
