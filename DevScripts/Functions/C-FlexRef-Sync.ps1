89ca202b35164f3f2516497c96760b63377aa365
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-22 13:03:01 +0100

    start using Compze.Build.FlexRef

---
+++ DevScripts/Functions/C-FlexRef-Sync.ps1
@@ -0,0 +1,20 @@
+# NOTE: Scripts that make changes should NOT log everything they do. They should only write output if something goes wrong.
+function C-FlexRef-Sync {
+    <#
+    .SYNOPSIS
+    Syncs FlexRef infrastructure for all solutions
+
+    .DESCRIPTION
+    Thin wrapper around 'flexref sync'. Updates Directory.Build.props,
+    all .csproj flex references, and NCrunch .v3.ncrunchsolution files.
+
+    .EXAMPLE
+    C-FlexRef-Sync
+    Syncs all FlexRef-managed files
+    #>
+    [CmdletBinding()]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs', '')]
+    param()
+
+    flexref sync $script:CompzeSrcRoot
+}
