60bce01a9c07c36000e27a73b63a6b077fbec7e8
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-23 16:33:17 +0100

    fix bug in Disposable

--- src/Compze.Utilities.SystemCE/Disposable..cs
+++ src/Compze.Utilities.SystemCE/Disposable..cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Compze.Utilities.Contracts;
 
 namespace Compze.Utilities.SystemCE;
@@ -6,17 +7,24 @@ namespace Compze.Utilities.SystemCE;
 ///<summary>Simple utility class that calls the supplied action when the instance is disposed. Gets rid of the need to create a ton of small classes to do cleanup.</summary>
 public class Disposable : IDisposable
 {
-   readonly Action _action;
+   int _ran = 0;
+   readonly Action _onDispose;
 
-   ///<summary>Constructs an instance that will call <param name="action"> when disposed.</param></summary>
-   public Disposable(Action action)
+   ///<summary>Constructs an instance that will call <param name="onDispose"> when disposed.</param></summary>
+   public Disposable(Action onDispose)
    {
-      Assert.Argument.NotNull(action);
-      _action = action;
+      Assert.Argument.NotNull(onDispose);
+      _onDispose = onDispose;
    }
 
    ///<summary>Invokes the action passed to the constructor.</summary>
-   public void Dispose() => _action();
+   public void Dispose()
+   {
+      if(_ran == 0 && Interlocked.CompareExchange(ref _ran, 1, 0) == 0)
+      {
+         _onDispose();
+      }
+   }
 
    public static readonly IDisposable NullOp = new Disposable(ActionCE.NullOp);
 }
