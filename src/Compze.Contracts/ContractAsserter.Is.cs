8eca4a1ecbd6faebe2b0ff7866b2794074f838ea
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-25 04:01:45 +0100

    refactoring

--- src/Compze.Contracts/ContractAsserter.Is.cs
+++ src/Compze.Contracts/ContractAsserter.Is.cs
@@ -9,14 +9,6 @@ public static class ContractAsserterIsExtensions
    extension(ContractAsserter @this)
    {
       [MethodImpl(MethodImplOptions.AggressiveInlining)]
-      public ContractAsserter Is([DoesNotReturnIf(false)] bool value,
-                                 [CallerArgumentExpression(nameof(value))] string valueString = "")
-      {
-         if(!value) @this.ThrowFailed(valueString);
-         return @this;
-      }
-
-      [MethodImpl(MethodImplOptions.AggressiveInlining)]
       public ContractAsserter NotDisposed([DoesNotReturnIf(true)] bool isDisposed, object theInstance)
       {
          ObjectDisposedException.ThrowIf(isDisposed, theInstance);
