2a2a7c2090b75cc53896175325678e34c52e19c4
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-26 13:19:02 +0100

    better messages on timeout waiting for endpoints to be at rest

--- src/Compze.Tessaging/Implementation/Transport/TessagesInFlightTracker.cs
+++ src/Compze.Tessaging/Implementation/Transport/TessagesInFlightTracker.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 using Compze.Core.Public;
 using Compze.Core.Refactoring.Naming.Internal;
 using Compze.Core.Tessaging.Hosting.Public;
@@ -22,14 +24,27 @@ public class TessagesInFlightTracker(ITypeMapper typeMapper) : ITessagesInFlight
    public void SendingTessageOnTransport(TransportTessage.OutGoing transportTessage, EndpointId remoteEndpointId) =>
       _implementation.Update(implementation => implementation.SendingTessageOnTransport(transportTessage, remoteEndpointId));
 
-   public void AwaitNoTessagesInFlight(TimeSpan? timeoutOverride) =>
-      _implementation.Await(implementation => implementation.NoTessagesInFlight(), timeoutOverride ?? 10.Seconds());
+   public void AwaitNoTessagesInFlight(TimeSpan? timeoutOverride)
+   {
+      try
+      {
+         _implementation.Await(implementation => implementation.NoTessagesInFlight(), timeoutOverride ?? 10.Seconds());
+      }
+      catch(AwaitingConditionTimeoutException e)
+      {
+         var diagnosticReport = _implementation.Read(implementation => implementation.CreateDiagnosticReport());
+         throw new AwaitingConditionTimeoutException(e, diagnosticReport);
+      }
+   }
 
    public void DoneWith(TransportTessage.InComing tessage, EndpointId handlingEndpointId, Exception? exception) =>
       _implementation.Update(implementation => implementation.DoneWith(tessage, handlingEndpointId, exception));
 
    public class InFlightTessage
    {
+      public required TessageId TessageId { get; init; }
+      public required string TypeName { get; init; }
+      public required string Body { get; init; }
       public Dictionary<EndpointId, bool> EndpointDeliveryStatus { get; } = [];
    }
 
@@ -44,7 +59,13 @@ public class TessagesInFlightTracker(ITypeMapper typeMapper) : ITessagesInFlight
 
       public void SendingTessageOnTransport(TransportTessage.OutGoing transportTessage, EndpointId remoteEndpointId)
       {
-         var inFlightTessage = TrackedTessages.GetOrAdd(transportTessage.TessageId, () => new InFlightTessage());
+         var inFlightTessage = TrackedTessages.GetOrAdd(transportTessage.TessageId,
+                                                        () => new InFlightTessage
+                                                              {
+                                                                 TessageId = transportTessage.TessageId,
+                                                                 TypeName = _typeMapper.GetType(transportTessage.Type).FullName ?? transportTessage.Type.ToString(),
+                                                                 Body = transportTessage.Body
+                                                              });
          inFlightTessage.EndpointDeliveryStatus[remoteEndpointId] = false;
       }
 
@@ -63,6 +84,66 @@ public class TessagesInFlightTracker(ITypeMapper typeMapper) : ITessagesInFlight
       }
 
       public bool NoTessagesInFlight() => TrackedTessages.Values.SelectMany(it => it.EndpointDeliveryStatus.Values).All(delivered => delivered);
+
+      public string CreateDiagnosticReport()
+      {
+         var sb = new StringBuilder();
+         sb.AppendLine("AwaitNoTessagesInFlight timed out.");
+
+         var undeliveredTessages = TrackedTessages.Values
+                                                  .Where(t => t.EndpointDeliveryStatus.Values.Any(delivered => !delivered))
+                                                  .ToList();
+
+         if(undeliveredTessages.Count > 0)
+         {
+            sb.AppendLine(CultureInfo.InvariantCulture,
+                          $"""
+
+                           ========== UNDELIVERED TESSAGES ({undeliveredTessages.Count}) ==========
+                           """);
+
+            foreach(var tessage in undeliveredTessages)
+            {
+               var pendingEndpoints = string.Join(", ", tessage.EndpointDeliveryStatus.Where(kvp => !kvp.Value).Select(kvp => kvp.Key));
+               sb.AppendLine(CultureInfo.InvariantCulture,
+                             $"""
+
+                              --- TessageId: {tessage.TessageId} ---
+                              Type: {tessage.TypeName}
+                              Pending endpoints: {pendingEndpoints}
+                              Body:
+                              {tessage.Body.Indent()}
+                              """);
+            }
+         } else
+         {
+            sb.AppendLine("""
+
+                          No undelivered tessages (all tessages were delivered before the diagnostic report was created).
+                          """);
+         }
+
+         if(_busExceptions.Count > 0)
+         {
+            sb.AppendLine(CultureInfo.InvariantCulture,
+                          $"""
+
+                           ========== BUS EXCEPTIONS ({_busExceptions.Count}) ==========
+                           """);
+
+            for(var i = 0; i < _busExceptions.Count; i++)
+            {
+               sb.AppendLine(CultureInfo.InvariantCulture,
+                             $"""
+
+                              --- Exception {i + 1} of {_busExceptions.Count} ---
+                              {_busExceptions[i]}
+                              """);
+            }
+         }
+
+         return sb.ToString();
+      }
    }
 }
 
