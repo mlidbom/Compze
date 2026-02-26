adc49136b0f593b588961d5ff232273242f8c2de
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-26 16:01:46 +0100

    Try to get better test output in github actions.

--- test/Compze.Tests.Infrastructure/XUnit/XUnitInfrastructureModuleInitializer.cs
+++ test/Compze.Tests.Infrastructure/XUnit/XUnitInfrastructureModuleInitializer.cs
@@ -1,6 +1,8 @@
 using Compze.Tessaging.Hosting.Testing;
 using Compze.Tests.Infrastructure.XUnit.Logging;
+using System;
 using System.Runtime.CompilerServices;
+using Compze.Utilities.Logging;
 using Compze.Utilities.Testing.XUnit.ComponentCombinations;
 
 namespace Compze.Tests.Infrastructure.XUnit;
@@ -10,7 +12,15 @@ public static class XUnitInfrastructureModuleInitializer
    [ModuleInitializer]
    public static void Initialize()
    {
-      TestFixtureHelper.SetupSerilog(new XUnitTestSerilogEnricher());
+      if(Environment.GetEnvironmentVariable("GITHUB_ACTIONS") == "true")
+      {
+         CompzeLogger.LogLevel = LogLevel.Debug;
+         CompzeLogger.LoggerFactoryMethod = XUnitTestOutputLogger.Create;
+      }
+      else
+      {
+         TestFixtureHelper.SetupSerilog(new XUnitTestSerilogEnricher());
+      }
 
       TestEnv.XunitDiscoverer = () => ComponentCombination.Current.ToPluggableComponents();
    }
