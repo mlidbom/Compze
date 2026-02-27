f13bf9182aea886abbc67407ff098f1efaee0630
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-27 06:03:26 +0100

    cruft

--- src/Compze.Contracts/AssertionTarget.cs
+++ src/Compze.Contracts/AssertionTarget.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+// ReSharper disable MemberCanBeInternal
 
 namespace Compze.Contracts;
 
