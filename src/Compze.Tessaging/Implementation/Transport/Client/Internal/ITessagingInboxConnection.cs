8b18a4afaade3de9657732f591bc6a65233306f1
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-27 07:14:42 +0100

    refactoring.

--- src/Compze.Tessaging/Implementation/Transport/Client/Internal/ITessagingInboxConnection.cs
+++ src/Compze.Tessaging/Implementation/Transport/Client/Internal/ITessagingInboxConnection.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks;
 using Compze.Core.Public;
 using Compze.Core.Tessaging.Public;
 using Compze.Tessaging.Implementation.Abstractions;
@@ -8,9 +7,6 @@ namespace Compze.Tessaging.Implementation.Transport.Client.Internal;
 public interface ITessagingInboxConnection
 {
     TessageTypesInternal.EndpointInformation EndpointInformation { get; }
-    Task SendAsync(IExactlyOnceTevent tevent);
-    Task SendAsync(IExactlyOnceTommand tommand);
-    void Deliver(TessageId tessageId, IExactlyOnceTessage tessage);
     void EnqueueForDelivery(TessageId tessageId, IExactlyOnceTessage tessage);
     void StartDelivery();
     void StopDelivery();
