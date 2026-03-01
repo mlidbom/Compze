cb41a0086a6524e97be34b1424c1b23c90967a55
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-01 15:37:40 +0100

    refactoring

--- src/Compze.Threading/ResourceAccess/AwaitableMonitorCEExtensions.cs
+++ src/Compze.Threading/ResourceAccess/AwaitableMonitorCEExtensions.cs
@@ -4,20 +4,23 @@ namespace Compze.Threading.ResourceAccess;
 
 static class AwaitableMonitorCEExtensions
 {
-   public static TResult ReadOrUpdate<TResult>(this IAwaitableMonitor @this, Func<TResult?> tryRead, Action updateOnFailedRead)
-      where TResult : class =>
-      @this.Read(() => tryRead() ?? @this.Update(() =>
-      {
-         updateOnFailedRead();
-         return tryRead() ?? throw new Exception($"{nameof(tryRead)} returned null even after {nameof(updateOnFailedRead)} had been called.");
-      }));
+   extension(IAwaitableMonitor @this)
+   {
+      public TResult ReadOrUpdate<TResult>(Func<TResult?> tryRead, Action updateOnFailedRead)
+         where TResult : class =>
+         @this.Read(() => tryRead() ?? @this.Update(() =>
+         {
+            updateOnFailedRead();
+            return tryRead() ?? throw new Exception($"{nameof(tryRead)} returned null even after {nameof(updateOnFailedRead)} had been called.");
+         }));
 
-   public static TResult ReadOrUpdate<TResult>(this IAwaitableMonitor @this, Func<bool> canRead, Func<TResult> read, Action update) =>
-      @this.Read(() => canRead()
-                          ? read()
-                          : @this.Update(() =>
-                          {
-                             update();
-                             return read();
-                          }));
+      public TResult ReadOrUpdate<TResult>(Func<bool> canRead, Func<TResult> read, Action update) =>
+         @this.Read(() => canRead()
+                             ? read()
+                             : @this.Update(() =>
+                             {
+                                update();
+                                return read();
+                             }));
+   }
 }
