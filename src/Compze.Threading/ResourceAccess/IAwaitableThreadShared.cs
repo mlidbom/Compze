203b45fbda7eeb148f59ef70150f190c44c7e7db
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-03 12:52:03 +0100

    refactoring

--- src/Compze.Threading/ResourceAccess/IAwaitableThreadShared.cs
+++ src/Compze.Threading/ResourceAccess/IAwaitableThreadShared.cs
@@ -20,6 +20,7 @@ public interface IAwaitableThreadShared<out TShared>
    TResult Update<TResult>(Func<TShared, TResult> update, LockTimeout? timeout = null);
    TResult UpdateWhen<TResult>(Func<TShared, bool> condition, Func<TShared, TResult> update, WaitTimeout? timeout = null);
 
+   //Default implementations
    unit Update(Action<TShared> update, LockTimeout? timeout = null) => Update(update.AsFunc(), timeout);
    unit UpdateWhen(Func<TShared, bool> condition, Action<TShared> update, WaitTimeout? timeout = null) => UpdateWhen(condition, update.AsFunc(), timeout);
 
