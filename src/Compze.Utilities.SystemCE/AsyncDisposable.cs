244f6fcaf9be56eac3b9e4ca0fa7d62e56a9eee6
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-03 14:39:35 +0100

    Cruft

--- src/Compze.Utilities.SystemCE/AsyncDisposable.cs
+++ src/Compze.Utilities.SystemCE/AsyncDisposable.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Compze.SystemCE.ThreadingCE.TasksCE;
 using Compze.Utilities.SystemCE.ThreadingCE;
 
+// ReSharper disable once ConvertToPrimaryConstructor
 namespace Compze.Utilities.SystemCE;
 
 ///<summary>Simple utility class that calls the supplied action when the instance is disposed. Gets rid of the need to create a ton of small classes to do cleanup.</summary>
@@ -12,11 +13,5 @@ sealed class AsyncDisposable : IAsyncDisposable
 
    public AsyncDisposable(Action dispose) => _dispose = dispose.AsAsync();
 
-   public AsyncDisposable(Func<Task> dispose) => _dispose = dispose;
-
-   public AsyncDisposable(Func<ValueTask> dispose) => _dispose = () => dispose().AsTask();
-
    public async ValueTask DisposeAsync() => await _dispose().caf();
-
-   public static readonly IAsyncDisposable NullOp = new AsyncDisposable(ActionCE.NullOp);
 }
