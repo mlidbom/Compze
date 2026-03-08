36385b99d842f463b2ca7862142178abcfa72414
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-08 15:10:38 +0100

    cruft

--- src/Compze.Threading/MutexCE.cs
+++ src/Compze.Threading/MutexCE.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Security.AccessControl;
 using System.Security.Principal;
-using System.Xml.Linq;
 
 namespace Compze.Threading;
 
