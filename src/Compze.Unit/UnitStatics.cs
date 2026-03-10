c72344ee875575dc1d06515374381b58e12bf8b6
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-11 00:08:11 +0100

    docs

--- src/Compze.Unit/UnitStatics.cs
+++ src/Compze.Unit/UnitStatics.cs
@@ -1,7 +1,12 @@
 namespace Compze.Unit;
 
-static class UnitStatics
+///<summary>
+/// Use <code>>global using static Compze.Unit.UnitStatics;</code> to make unit available as a value globally.
+///then you can write just <code>return unit;</code> instead of <code>return Unit.Value;</code>
+/// </summary>
+public static class UnitStatics
 {
    // ReSharper disable once InconsistentNaming
+   /// <summary>alias for Unit.Value intended to be used via <code>>global using static Compze.Unit.UnitStatics;</code></summary>
    public static readonly Unit unit = Unit.Value;
 }
