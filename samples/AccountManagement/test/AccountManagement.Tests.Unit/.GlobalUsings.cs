9cb8f48df8ce19aca2c525450a708c1134fa3c2b
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-11 00:09:49 +0100

    checkpoint

--- samples/AccountManagement/test/AccountManagement.Tests.Unit/.GlobalUsings.cs
+++ samples/AccountManagement/test/AccountManagement.Tests.Unit/.GlobalUsings.cs
@@ -1,5 +1,5 @@
 #pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
-global using unit = Compze.Unit.Unit;
+global using static Compze.Unit.UnitStatics;
 global using Compze.Unit;
 global using Compze.Underscore;
 global using static Compze.Contracts.Contract;
