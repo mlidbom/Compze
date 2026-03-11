b8963a4e11e62747fcebae7511f53f725546fbb5
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-12 00:04:15 +0100

    Docs.

--- src/Compze.Threading.Testing/TestingTaskRunner.cs
+++ src/Compze.Threading.Testing/TestingTaskRunner.cs
@@ -10,8 +10,10 @@ public sealed class TestingTaskRunner(TimeSpan timeout) : IDisposable, IAsyncDis
    readonly List<Task> _monitoredTasks = [];
    readonly TimeSpan _timeout = timeout;
 
+   ///<summary>Returns an instance that throws an exception on <see cref="Dispose"/> if all tasks do not complete within <paramref name="timeout"/></summary>
    public static TestingTaskRunner WithTimeout(TimeSpan timeout) => new(timeout);
 
+   ///<summary>Starts the supplied action as a background <see cref="Task"/> and returns that task. Tracks all the started tasks and throws an exception on <see cref="Dispose"/> if any are failed</summary>
    public Task Run(Action action)
    {
       var task = TaskCE.Run(action);
@@ -19,9 +21,12 @@ public sealed class TestingTaskRunner(TimeSpan timeout) : IDisposable, IAsyncDis
       return task;
    }
 
+   ///<summary>Starts the supplied actions as a background <see cref="Task"/> and returns the list of tasks. Tracks all the started tasks and throws an exception on <see cref="Dispose"/> if any are failed</summary>
    public IReadOnlyList<Task> Run(params Action[] tasks) => tasks.Select(Run).ToList();
 
+   ///<summary>Waits for all the registered tasks to complete and throws an exception if any are failed or the timeout expires.</summary>
    public void Dispose() => DisposeAsync().WaitUnwrappingException();
+   ///<summary>Waits for all the registered tasks to complete and throws an exception if any are failed or the timeout expires.</summary>
    public async ValueTask DisposeAsync() => await WaitForTasksToCompleteAsync().caf();
 
    async Task WaitForTasksToCompleteAsync() => await Task.WhenAll(_monitoredTasks.ToArray()).WaitAsync(_timeout).caf();
