9033242150e18f96a12f50a7d51484214004c31b
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-15 00:01:38 +0100

    adjust timings

--- test/Compze.Tests.Performance.Internals/Tessaging/Hypermedia/Local_Tuery_Performance_tests.cs
+++ test/Compze.Tests.Performance.Internals/Tessaging/Hypermedia/Local_Tuery_Performance_tests.cs
@@ -11,8 +11,8 @@ public class Local_Tuery_performance_tests : PerformanceTestBase
    [PCT]  public void Runs_10_000__MultiThreaded_local_requests_making_a_single_local_tuery_each_in_30_milliseconds() =>
       RunScenario(threaded: true, requests: 10_000.EnvDivide(instrumented:12), tueriesPerRequest: 1, maxTotal: 30.Milliseconds());
 
-   [PCT]  public void Runs_10_000_SingleThreaded_local_requests_making_a_single_local_tuery_each_in_80_milliseconds() =>
-      RunScenario(threaded: false, requests: 10_000.EnvDivide(instrumented:6), tueriesPerRequest: 1, maxTotal: 80.Milliseconds());
+   [PCT]  public void Runs_10_000_SingleThreaded_local_requests_making_a_single_local_tuery_each_in_140_milliseconds() =>
+      RunScenario(threaded: false, requests: 10_000.EnvDivide(instrumented:4), tueriesPerRequest: 1, maxTotal: 140.Milliseconds());
 
    [PCT]  public void Runs_4_000__MultiThreaded_local_requests_making_10_local_tueries_each_in_50_milliseconds() =>
       RunScenario(threaded: true, requests: 4_000.EnvDivide(instrumented:12), tueriesPerRequest: 10, maxTotal: 50.Milliseconds());
