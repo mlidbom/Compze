3563079b20e58a09c1985f8ef9bf3a45514ddec6
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-15 22:43:14 +0100

    Remove duplicate tests.

--- test/Compze.Threading.Specifications/IAwaitableShared_/IAwaitableProcessShared_/IProcessShared_specification.cs
+++ test/Compze.Threading.Specifications/IAwaitableShared_/IAwaitableProcessShared_/IProcessShared_specification.cs
@@ -12,26 +12,6 @@ public class IProcessShared_specification : UniversalTestBase
 {
    public class Global : IProcessShared_specification
    {
-      [XF] public void returns_the_value_from_the_locked_function()
-      {
-         var shared = IProcessShared.Global("IProcessShared_specification.Global.returns_value", 42, timeout: null, onAbandonedMutexException: null);
-         shared.Locked(value => value).Must().Be(42);
-      }
-
-      [XF] public void provides_the_shared_value_to_the_function()
-      {
-         var shared = IProcessShared.Global("IProcessShared_specification.Global.provides_shared", "hello", timeout: null, onAbandonedMutexException: null);
-         shared.Locked(value => value).Must().Be("hello");
-      }
-
-      [XF] public void executes_the_action_with_the_shared_value()
-      {
-         var list = new List<int>();
-         var shared = IProcessShared.Global("IProcessShared_specification.Global.executes_action", list, timeout: null, onAbandonedMutexException: null);
-         shared.Locked(value => value.Add(42));
-         list.Must().HaveCount(1);
-      }
-
       [XF] public void Mutex_IsGlobal_is_true()
       {
          var shared = IProcessShared.Global("IProcessShared_specification.Global.Mutex_IsGlobal", 0, timeout: null, onAbandonedMutexException: null);
@@ -53,26 +33,6 @@ public class IProcessShared_specification : UniversalTestBase
 
    public class Local : IProcessShared_specification
    {
-      [XF] public void returns_the_value_from_the_locked_function()
-      {
-         var shared = IProcessShared.Local("IProcessShared_specification.Local.returns_value", 42, timeout: null, onAbandonedMutexException: null);
-         shared.Locked(value => value).Must().Be(42);
-      }
-
-      [XF] public void provides_the_shared_value_to_the_function()
-      {
-         var shared = IProcessShared.Local("IProcessShared_specification.Local.provides_shared", "hello", timeout: null, onAbandonedMutexException: null);
-         shared.Locked(value => value.Length).Must().Be(5);
-      }
-
-      [XF] public void executes_the_action_with_the_shared_value()
-      {
-         var list = new List<int>();
-         var shared = IProcessShared.Local("IProcessShared_specification.Local.executes_action", list, timeout: null, onAbandonedMutexException: null);
-         shared.Locked(value => value.Add(42));
-         list.Must().HaveCount(1);
-      }
-
       [XF] public void Mutex_IsGlobal_is_false()
       {
          var shared = IProcessShared.Local("IProcessShared_specification.Local.Mutex_IsGlobal", 0, timeout: null, onAbandonedMutexException: null);
