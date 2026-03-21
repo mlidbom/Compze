7a3729ef897bf59fc2703ba863e877a0c4251997
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-03-21 14:37:51 +0100

    Add compliance tests.

---
+++ test/Compze.DependencyInjection.MsDiCompliance/CompzeLightInjectMsDiComplianceTests.cs
@@ -0,0 +1,22 @@
+using Compze.DependencyInjection.Abstractions;
+using Compze.DependencyInjection.LightInject;
+using Compze.DependencyInjection.LightInject.Extensions.Hosting;
+using LightInject;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Specification;
+using ContainerOptions = Compze.DependencyInjection.Abstractions.ContainerOptions;
+
+namespace Compze.DependencyInjection.MsDiCompliance;
+
+public class CompzeLightInjectMsDiComplianceTests : DependencyInjectionSpecificationTests
+{
+   static readonly ContainerOptions ComplianceOptions = new() { AllowScopedResolutionFromRoot = true };
+
+   protected override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
+   {
+      var builder = new LightInjectContainerBuilder();
+      var factory = new CompzeLightInjectServiceProviderFactory(builder, ComplianceOptions);
+      var lightInjectContainer = factory.CreateBuilder(serviceCollection);
+      return factory.CreateServiceProvider(lightInjectContainer);
+   }
+}
