951f6f4dd20d624bea2d0b5bfa7cc0b2fc04a2f3
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-05-18 20:04:49 +0200

    Harmonize encodings.

--- src/Compze.Threading/IUpdateLock.cs
+++ src/Compze.Threading/IUpdateLock.cs
@@ -1,4 +1,4 @@
-﻿namespace Compze.Threading;
+namespace Compze.Threading;
 
 ///<summary>An update lock for an <see cref="IAwaitableCriticalSection"/>. Calling dispose releases the lock and notifies all threads waiting on conditions to become true to reacquire the lock and reevaluate the conditions.</summary>
 public interface IUpdateLock : IDisposable
