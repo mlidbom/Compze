e2f67aa3fafec21125dc4ef353a5b84f69345f97
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-06-22 23:20:08 +0200

    Remove Compze.Core project.

--- src/Compze.Core/.GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
-global using static Compze.SystemCE.UnitStatics;
-global using Compze.Underscore;
-global using Compze.Internals.SystemCE;
-global using Compze.Internals.SystemCE.LinqCE;
-global using static Compze.Contracts.Contract;
