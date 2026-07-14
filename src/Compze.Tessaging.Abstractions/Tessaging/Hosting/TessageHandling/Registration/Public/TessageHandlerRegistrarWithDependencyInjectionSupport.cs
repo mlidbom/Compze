fadc4785467537f9129b430206961b19fb337fff
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-07-15 00:16:02 +0200

    Todo comment

--- src/Compze.Tessaging.Abstractions/Tessaging/Hosting/TessageHandling/Registration/Public/TessageHandlerRegistrarWithDependencyInjectionSupport.cs
+++ src/Compze.Tessaging.Abstractions/Tessaging/Hosting/TessageHandling/Registration/Public/TessageHandlerRegistrarWithDependencyInjectionSupport.cs
@@ -1,5 +1,6 @@
 namespace Compze.Tessaging.Abstractions.Tessaging.Hosting.TessageHandling.Registration.Public;
 
+//todo:urgent:  This adds literally nothing to a raw ITessageHandlerRegistrar. Remove it. 
 public class TessageHandlerRegistrarWithDependencyInjectionSupport(ITessageHandlerRegistrar registrar)
 {
    internal ITessageHandlerRegistrar Registrar { get; } = registrar;
